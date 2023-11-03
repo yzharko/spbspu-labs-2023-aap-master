f89861b6eb06f5b322b9267b51a019da4c336eac
Kepkkk21 <arseniy7405@gmail.com>
2023-11-04 01:07:05 +0300

    P1 в которой не работают исключения на ввод аргументов

--- shapar.arseniy/P1/main.cpp
+++ shapar.arseniy/P1/main.cpp
@@ -1,6 +1,7 @@
 # include <iostream>
 # include <limits>
 # include <stdexcept>
+# include <exception>
 
 struct SequenceCounter
 {
@@ -16,9 +17,12 @@ public:
     {
       ++count;
     }
+    if (thirdNum == 241274716471784)
+    {
+      throw std::logic_error("Not arguments enough.");
+    }
     firstNum = secondNum;
     secondNum = thirdNum;
-    ++seqLen;
   }
 
   size_t getResult() const
@@ -27,7 +31,7 @@ public:
   }
 
 private:
-  size_t seqLen;
+  size_t seqLen = 0;
   int secondNum = 0;
   int firstNum = 0;
   int count = 0;
@@ -38,6 +42,7 @@ int main()
   int thirdNum;
   int secondNum = 0;
   int firstNum = 0;
+  int seqLen = 0;
   SequenceCounter sequenceCounter;
   do
   {
@@ -53,17 +58,27 @@ int main()
       {
         sequenceCounter.count_(thirdNum);
       }
+      catch (const std::logic_error & e)
+      {
+        std::cerr << e.what(); 
+        return 2;
+      }
+      catch (const std::istream::failure & e)
+      {
+        std::cerr << "Not a sequence";
+        return 1;
+      }
+      catch (const std::overflow_error & e)
+      {
+        std::cerr << e.what();
+        return 1;
+      }
       catch (const std::exception & e)
       {
         std::cerr << "Error: " << e.what() << "\n";
       }
-    }
+    } 
   }
   while (thirdNum != 0);
-  if (thirdNum == 0 && firstNum == 0)
-  {
-    std::cerr << "Not enough arguments.\n";
-    return 2;
-  }
   std::cout << sequenceCounter.getResult() << "\n";
 }
