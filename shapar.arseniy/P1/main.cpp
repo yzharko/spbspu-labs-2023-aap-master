53608122fff6944c3f4ef8f9540355fe9795f95f
Kepkkk21 <arseniy7405@gmail.com>
2023-11-03 20:43:39 +0300

    P1 test 2

--- shapar.arseniy/P1/main.cpp
+++ shapar.arseniy/P1/main.cpp
@@ -1,6 +1,42 @@
 # include <iostream>
+# include <limits>
 
 int main()
 {
-  std::cout<<"Hello";
+  int firstNum = 0;
+  int secondNum = 0;
+  int thirdNum = 0;
+  int count = 0;
+  size_t seqLen = 0;
+  do
+  {
+    std::cin >> thirdNum;
+    if (!std::cin)
+    {
+      std::cerr << "Not a sequence.\n";
+      return 1;
+    }
+    else if (thirdNum != 0)
+    {
+      const size_t maxSize = std::numeric_limits< size_t >::max();
+      if (seqLen == maxSize)
+      {
+        std::cerr << "Sequence is too long.\n";
+        return 2;
+      }
+      if ( thirdNum == secondNum + firstNum)
+       {
+         ++count;
+       }
+       firstNum = secondNum;
+       secondNum = thirdNum;
+       ++seqLen;
+    }
+  }
+  while (thirdNum != 0);
+  if (seqLen == 1 || seqLen == 2 || seqLen == 3)
+  {
+    std::cerr << "Error";
+  }
+  std::cout << count << "\n";
 }
