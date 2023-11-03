a3acc85e267cd69ea67c1735cee221b0fef0d3f0
Kepkkk21 <arseniy7405@gmail.com>
2023-11-03 23:09:57 +0300

    P1 работает, но по какой то причине всегда выводит 'Not enough arguments'

--- shapar.arseniy/P1/main.cpp
+++ shapar.arseniy/P1/main.cpp
@@ -1,13 +1,44 @@
 # include <iostream>
 # include <limits>
+# include <stdexcept>
+
+struct SequenceCounter
+{
+public:
+  void count_(int thirdNum)
+  {
+    const size_t maxSize = std::numeric_limits< size_t >::max();
+    if (seqLen == maxSize)
+    {
+      throw std::logic_error("Sequence is too long.");
+    }
+    if (thirdNum == secondNum + firstNum)
+    {
+      ++count;
+    }
+    firstNum = secondNum;
+    secondNum = thirdNum;
+    ++seqLen;
+  }
+
+  size_t getResult() const
+  {
+    return count;
+  }
+
+private:
+  size_t seqLen;
+  int secondNum = 0;
+  int firstNum = 0;
+  int count = 0;
+};
 
 int main()
 {
-  int firstNum = 0;
+  int thirdNum;
   int secondNum = 0;
-  int thirdNum = 0;
-  int count = 0;
-  size_t seqLen = 0;
+  int firstNum = 0;
+  SequenceCounter sequenceCounter;
   do
   {
     std::cin >> thirdNum;
@@ -18,25 +49,21 @@ int main()
     }
     else if (thirdNum != 0)
     {
-      const size_t maxSize = std::numeric_limits< size_t >::max();
-      if (seqLen == maxSize)
+      try
       {
-        std::cerr << "Sequence is too long.\n";
-        return 2;
+        sequenceCounter.count_(thirdNum);
       }
-      if ( thirdNum == secondNum + firstNum)
-       {
-         ++count;
-       }
-       firstNum = secondNum;
-       secondNum = thirdNum;
-       ++seqLen;
+      catch (const std::exception & e)
+      {
+        std::cerr << "Error: " << e.what() << "\n";
+      }
     }
   }
   while (thirdNum != 0);
-  if (seqLen == 1 || seqLen == 2 || seqLen == 3)
+  if (thirdNum == 0 && firstNum == 0)
   {
-    std::cerr << "Error";
+    std::cerr << "Not enough arguments.\n";
+    return 2;
   }
-  std::cout << count << "\n";
+  std::cout << sequenceCounter.getResult() << "\n";
 }
