aa039a9bce5e7d01bc53d4771e6f7714a2806da1
yva <yartsev-va@yandex.ru>
2023-11-12 23:43:16 +0300

    Implemented simple input for num == 1

--- yartsev.viktor/P4/main.cpp
+++ yartsev.viktor/P4/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char ** argv)
 {
@@ -9,16 +10,37 @@ int main(int argc, char ** argv)
     std::cerr << "Too many arguments\n";
     return 1;
   }
+
+  int num = 0;
   try {
-    int num = std::stol(argv[1]);
+    num = std::stol(argv[1]);
     if (num < 1 || num > 2) {
-        std::cerr << "First parameter is out of range\n";
-        return 1;
+      std::cerr << "First parameter is out of range\n";
+      return 1;
     }
   } catch (std::invalid_argument & e) {
     std::cerr << "First parameter is not a number\n";
     return 1;
   }
-  std::cout << "OK\n";
+
+  if (num == 1) {
+    int a[10000] = {};
+    int rows = 0, cols = 0;
+    std::ifstream input(argv[2]);
+    input >> rows >> cols;
+    if (rows != cols) {
+      std::cout << "Not a triangular matrix\n";
+      return 0;
+    }
+    for (int i = 0; i < rows * cols; i++) {
+      input >> a[i];
+      if (!input) {
+        std::cerr << "Not a matrix\n";
+        return 2;
+      }
+    }
+    bool isZero = true;
+    
+  }
   return 0;
 }
