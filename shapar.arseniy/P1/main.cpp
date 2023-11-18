cba3cdfce99496991d6ff9789d324bd8dbe877eb
Kepkkk21 <arseniy7405@gmail.com>
2023-11-18 16:17:33 +0300

    подумал, что ошибка контента выходит из за названия файла, загрузил  файл main1.cpp под другим именем

--- shapar.arseniy/P1/main.cpp
+++ shapar.arseniy/P1/main.cpp
@@ -1,63 +1,28 @@
 # include <iostream>
-# include <limits>
 # include <stdexcept>
 # include <exception>
-
-struct SequenceCounter
-{
-public:
-  void count_(int fourthNum)
-  {
-    const size_t maxSize = std::numeric_limits< size_t >::max();
-    if (seqLen == maxSize)
-    {
-      throw std::logic_error("Sequence is too long.");
-    }
-    if (secondNum < firstNum && secondNum > thirdNum)
-    {
-      ++count;
-    }
-    firstNum = secondNum;
-    secondNum = thirdNum;
-    thirdNum = fourthNum;
-    }
-
-  size_t getResult() const
-  {
-    return count;
-  }
-
-private:
-  size_t seqLen = 0;
-  int secondNum = 0;
-  int firstNum = 0;
-  int fourthNum = 0;
-  int thirdNum = 0;
-  int count = 0;
-};
+#include "var1.hpp"
 
 int main()
 {
-  int thirdNum = 0;
-  int secondNum = 0;
-  int firstNum = 0;
   int seqLen = 0;
-  int fourthNum = 0;
+  int thirdNum;
+  using namespace shapar;
   SequenceCounter sequenceCounter;
   do
   { 
     ++seqLen;
-    std::cin >> fourthNum;
+    std::cin >> thirdNum;
     if (!std::cin)
     {
       std::cerr << "Not a sequence.\n";
       return 1;
     }
-    else if (fourthNum != 0)
+    else if (thirdNum != 0)
     {
       try
       {
-        sequenceCounter.count_(fourthNum);
+        sequenceCounter(thirdNum);
       }
       catch (const std::logic_error & e)
       {
@@ -80,6 +45,11 @@ int main()
       }
     } 
   }
-  while (fourthNum != 0);
-  std::cout << sequenceCounter.getResult() << "\n";
+  while (thirdNum != 0);
+  if (seqLen <= 3)
+  {
+    std::cerr<<"Not enough arguments";
+    return 2;
+  }
+  std::cout << sequenceCounter() << "\n";
 }
