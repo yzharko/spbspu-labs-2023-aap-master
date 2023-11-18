97f65657910410457a35c030dbc0460abf263d28
Kepkkk21 <arseniy7405@gmail.com>
2023-11-18 14:58:00 +0300

    1var-P1 с перегрузкой

---
+++ shapar.arseniy/P1/main1.cpp
@@ -0,0 +1,90 @@
+
+# include <iostream>
+# include <limits>
+# include <stdexcept>
+# include <exception>
+
+struct SequenceCounter
+{
+public:
+  SequenceCounter():
+    seqLens(0)
+  {}
+  void operator()(int thirdNum)
+  {
+    const size_t maxSize = std::numeric_limits< size_t >::max();
+    if (seqLens == maxSize)
+    {
+      throw std::logic_error("Sequence is too long.");
+    }
+    if (thirdNum == secondNum + firstNum)
+    {
+      ++count;
+    }
+    firstNum = secondNum;
+    secondNum = thirdNum;
+  }
+
+  size_t operator()() const
+  {
+    return count;
+  }
+
+private:
+  size_t seqLens;
+  int secondNum;
+  int firstNum;
+  int count = 0;
+};
+
+int main()
+{
+  int thirdNum;
+  int secondNum = 0;
+  int firstNum = 0;
+  int seqLen = 0;
+  SequenceCounter sequenceCounter;
+  do
+  { 
+    ++seqLen;
+    std::cin >> thirdNum;
+    if (!std::cin)
+    {
+      std::cerr << "Not a sequence.\n";
+      return 1;
+    }
+    else if (thirdNum != 0)
+    {
+      try
+      {
+        sequenceCounter(thirdNum);
+      }
+      catch (const std::logic_error & e)
+      {
+        std::cerr << e.what(); 
+        return 2;
+      }
+      catch (const std::istream::failure & e)
+      {
+        std::cerr << "Not a sequence";
+        return 1;
+      }
+      catch (const std::overflow_error & e)
+      {
+        std::cerr << e.what();
+        return 1;
+      }
+      catch (const std::exception & e)
+      {
+        std::cerr << "Error: " << e.what() << "\n";
+      }
+    } 
+  }
+  while (thirdNum != 0);
+  if (seqLen <= 3)
+  {
+    std::cerr<<"Not enough arguments";
+    return 2;
+  }
+  std::cout << sequenceCounter() << "\n";
+}
