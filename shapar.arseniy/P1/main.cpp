13a01417e5d42edceeabc75a8570a6b5a0a097db
Kepkkk21 <arseniy7405@gmail.com>
2023-11-26 17:45:46 +0300

    test3

--- shapar.arseniy/P1/main.cpp
+++ shapar.arseniy/P1/main.cpp
@@ -2,12 +2,54 @@
 #include <exception>
 #include <cstddef>
 #include "var2.hpp"
+#include "var1.hpp"
 
 int main()
 {
   size_t fourthNum_ = 0;
+  size_t thirdNum = 0;
   using namespace shapar_;
+  using namespace shapar;
+  SequenceCounter sequenceCounter;
   SequenceCounter_ sequenceCounter_;
+  do
+  {
+    std::cin >> thirdNum;
+    if (!std::cin)
+    {
+      std::cerr << "Not a sequence.\n";
+      return 1;
+    }
+    else if (thirdNum != 0)
+    {
+      try
+      {
+        sequenceCounter(thirdNum);
+      }
+      catch (const std::logic_error & e)
+      {
+        std::cerr << e.what();
+        return 2;
+      }
+      catch (const std::istream::failure & e)
+      {
+        std::cerr << "Not a sequence";
+        return 1;
+      }
+      catch (const std::overflow_error & e)
+      {
+        std::cerr << e.what();
+        return 1;
+      }
+      catch (const std::exception & e)
+      {
+        std::cerr << "Error: " << e.what() << "\n";
+        return 2;
+      }
+    }
+  }
+  while (thirdNum != 0);
+  std::cout << sequenceCounter() << "\n";
   do
   {
     std::cin >> fourthNum_;
--- shapar.arseniy/P1/var1.cpp
+++ shapar.arseniy/P1/var1.cpp
@@ -1,4 +1,3 @@
-
 #include <limits>
 #include <stdexcept>
 #include "var1.hpp"
