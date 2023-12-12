c1f2bb4cb7ff5992144213ed8b044de1b7328919
shavel0k <anna.reznikova@icloud.com>
2023-12-12 23:43:27 +0300

    added implementation of other methods of the rectangle class

--- reznikova.anna/T1/rectangle.cpp
+++ reznikova.anna/T1/rectangle.cpp
@@ -8,12 +8,6 @@ Rectangle::Rectangle(double lx, double ly, double rx, double ry):
   rp_y(ry)
 {}
 
-void Rectangle::getShape()
-{
-  std::cout << "left bottom point: (" << lp_x << ", " << lp_y << ")\n";
-  std::cout << "right upper point: (" << rp_x << ", " << rp_y << ")\n";
-}
-
 double Rectangle::getArea()
 {
   double Area = 0;
@@ -31,3 +25,33 @@ rectangle_t Rectangle::getFrameRect()
   rectangle_t rectangle(width, height, pos);
   return rectangle;
 }
+
+void Rectangle::move(double dx, double dy)
+{
+  lp_x += dx;
+  rp_x += dx;
+  lp_y += dy;
+  rp_y += dy;
+}
+
+void Rectangle::move(point_t new_center)
+{
+  double new_x0 = new_center.x;
+  double new_y0 = new_center.y;
+  double old_x0 = (rp_x + lp_x)/2;
+  double old_y0 = (rp_y + lp_y)/2;
+  double dx = (new_x0 - old_x0);
+  double dy = (new_y0 - old_y0);
+  move(dx, dy);
+}
+
+void Rectangle::scale(double n)
+{
+  double width = std::abs(rp_x - lp_x);
+  double height = std::abs(rp_y - lp_y);
+  lp_x -= width/2*(n-1);
+  lp_y -= height/2*(n-1);
+  rp_x += width/2*(n-1);
+  rp_y += height/2*(n-1);
+}
+
