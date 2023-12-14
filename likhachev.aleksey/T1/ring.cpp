24929c3cd8b28acd95629b9a90d8e0d8fe265d87
Likhachev Aleksey <lavran03@gmail.com>
2023-12-15 02:48:04 +0300

    (stage) Объявление и определение структуры Ring

---
+++ likhachev.aleksey/T1/ring.cpp
@@ -0,0 +1,64 @@
+#include "ring.hpp"
+
+likhachev::Ring::Ring():
+  pos_(Point_t(0.0, 0.0)),
+  outerRad_(1.0),
+  innerRad_(0.5)
+{}
+
+likhachev::Ring::Ring(Point_t pos, double outerRad, double innerRad):
+  pos_(pos),
+  outerRad_(outerRad),
+  innerRad_(innerRad)
+{}
+
+likhachev::Ring::Ring(double posX, double posY, double outerRad, double innerRad):
+  pos_(Point_t(posX, posY)),
+  outerRad_(outerRad),
+  innerRad_(innerRad)
+{}
+
+double likhachev::Ring::getArea() const
+{
+  const double PI = 3.141592653589793;
+  double outerArea = PI * outerRad_;
+  double innerArea = PI * innerRad_;
+
+  return outerArea - innerArea;
+}
+
+likhachev::Rectangle_t likhachev::Ring::getFrameRect() const
+{
+  double width = outerRad_;
+  double height = outerRad_;
+  Point_t pos(pos_);
+  
+  return Rectangle_t(width, height, pos);
+}
+
+void likhachev::Ring::move(Point_t offset)
+{
+  pos_ += offset;
+}
+
+void likhachev::Ring::move(double offsetX, double offsetY)
+{
+   Point_t offset(offsetX, offsetY);
+   move(offset);
+}
+
+void likhachev::Ring::scale(double multiplier)
+{
+  outerRad_ *= multiplier;
+  innerRad_ *= multiplier;
+}
+
+double likhachev::Ring::getOuterRad() const
+{
+  return outerRad_;
+}
+
+double likhachev::Ring::getInnerRad() const
+{
+  return innerRad_;
+}
---
+++ likhachev.aleksey/T1/ring.hpp
@@ -0,0 +1,26 @@
+#ifndef RING_HPP
+#define RING_HPP
+#include "base-types.hpp"
+#include "shape.hpp"
+
+namespace likhachev
+{
+  class Ring: public Shape {
+    public:
+      Ring();
+      Ring(Point_t pos, double outerRad, double innerRad);
+      Ring(double posX, double posY, double outerRad, double innerRad);
+      virtual double getArea() const;
+      virtual likhachev::Rectangle_t getFrameRect() const;
+      virtual void move(Point_t offset);
+      virtual void move(double offsetX, double offsetY);
+      virtual void scale(double multiplier);
+      double getOuterRad() const;
+      double getInnerRad() const;
+    private:
+      double outerRad_;
+      double innerRad_;
+      Point_t pos_; 
+  };
+}
+#endif
