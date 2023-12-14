80d7e7594c00db068cf1b39f7f15a3970d918c19
Likhachev Aleksey <lavran03@gmail.com>
2023-12-15 01:24:09 +0300

    (stage) Определение базовых элементов
    Добавлены объявление базовых элементов "Point_t" и "Rectangle_t", а так-же создан файл .cpp для их определения

---
+++ likhachev.aleksey/T1/base-types.cpp
@@ -0,0 +1,91 @@
+#include "base-types.hpp"
+#include <iostream>
+
+likhachev::Point_t::Point_t():
+  x(0.0),
+  y(0.0)
+{}
+
+likhachev::Point_t::Point_t(double newX, double newY):
+  x(newX),
+  y(newY)
+{}
+
+likhachev::Point_t likhachev::Point_t::operator+(const Point_t& rhs)
+{
+  x += rhs.x;
+  y += rhs.y;
+  return *this;
+}
+
+likhachev::Point_t likhachev::Point_t::operator-(const Point_t& rhs)
+{
+  x -= rhs.x;
+  y -= rhs.y;
+  return *this;
+}
+
+likhachev::Point_t& likhachev::Point_t::operator+=(const Point_t& rhs)
+{
+  *this = *this + rhs;
+  return *this;
+}
+
+likhachev::Point_t& likhachev::Point_t::operator-=(const Point_t& rhs)
+{
+  *this = *this - rhs;
+  return *this;
+}
+
+likhachev::Rectangle_t::Rectangle_t():
+  width_(0),
+  height_(0),
+  pos_(Point_t())
+{}
+
+likhachev::Rectangle_t::Rectangle_t(double width, double height, Point_t pos):
+  width_(width),
+  height_(height),
+  pos_(pos)
+{}
+
+double likhachev::Rectangle_t::getArea() const
+{
+  return width_ * height_;
+}
+
+double likhachev::Rectangle_t::getWidth() const
+{
+  return width_;
+}
+
+double likhachev::Rectangle_t::getHeight() const
+{
+  return height_;
+}
+
+likhachev::Point_t likhachev::Rectangle_t::getPos() const
+{
+  return pos_;
+}
+
+void likhachev::Rectangle_t::setWidth(double width)
+{
+  if (width <= 0) {
+    throw std::logic_error("Error: the width of the figure cannot be non-positive\n");
+  }
+  width_ = width;
+}
+
+void likhachev::Rectangle_t::setHeight(double height)
+{
+  if (height <= 0) {
+    throw std::logic_error("Error: the height of the figure cannot be non-positive\n");
+  }
+  height_ = height;
+}
+
+void likhachev::Rectangle_t::setPos(Point_t pos)
+{
+  pos_ = pos;
+}
---
+++ likhachev.aleksey/T1/base-types.hpp
@@ -0,0 +1,36 @@
+#ifndef BASE_TYPES_HPP
+#define BASE_TYPES_HPP
+
+namespace likhachev
+{
+  struct Point_t {
+    public:
+      double x;
+      double y;
+      Point_t();
+      Point_t(double newX, double newY);
+      void operator()(double newNumber);
+      Point_t operator+(const Point_t& rhs);
+      Point_t operator-(const Point_t& rhs);
+      Point_t& operator+=(const Point_t& rhs);
+      Point_t& operator-=(const Point_t& rhs);
+  };
+
+  struct Rectangle_t {
+    public:
+      Rectangle_t();
+      Rectangle_t(double width, double height, Point_t pos);
+      double getArea() const;
+      double getWidth() const;
+      double getHeight() const;
+      likhachev::Point_t getPos() const;
+      void setWidth(double width);
+      void setHeight(double height);
+      void setPos(Point_t pos);
+    private:
+      double width_;
+      double height_;
+      Point_t pos_;
+  };
+}
+#endif
