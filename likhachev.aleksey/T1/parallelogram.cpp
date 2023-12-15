ae95c5befe2b999d2a88266e26f63005538ac0e4
Likhachev Aleksey <lavran03@gmail.com>
2023-12-15 04:01:32 +0300

    (stage) Определение и объявление класса Parallelogram
    Изменить класс после того, как будет дано добро

---
+++ likhachev.aleksey/T1/parallelogram.cpp
@@ -0,0 +1,141 @@
+#include "parallelogram.hpp"
+
+likhachev::Parallelogram::Parallelogram():
+  pointA_(Point_t(2.0, 1.0)),
+  pointB_(Point_t(-1.0, 1.0)),
+  pointD_(Point_t(1.0, -1.0))
+  
+{}
+
+likhachev::Parallelogram::Parallelogram(Point_t pointA, Point_t pointB, Point_t pointD):
+  pointA_(pointA),
+  pointB_(pointB),
+  pointD_(pointD)
+{}
+
+likhachev::Parallelogram::Parallelogram(double pointAX, double pointAY, double pointBX, double pointBY, double pointDX, double pointDY):
+  pointA_(Point_t(pointAX, pointAY)),
+  pointB_(Point_t(pointBX, pointBY)),
+  pointD_(Point_t(pointDX, pointDY))
+{}
+
+double likhachev::Parallelogram::getArea() const
+{
+  return getWidth() * getHeight();
+}
+
+likhachev::Rectangle_t likhachev::Parallelogram::getFrameRect() const
+{
+  double fullWidth = getWidth() + getSubWidth();
+  double height = getHeight();
+  Point_t pos((pointB_.x + pointD_.x) / 2, (pointB_.y + pointD_.y) / 2);
+  
+  return Rectangle_t(fullWidth, height, pos);
+}
+
+void likhachev::Parallelogram::move(Point_t offset)
+{
+  pointA_ += offset;
+  pointB_ += offset;
+  pointD_ += offset;
+}
+
+void likhachev::Parallelogram::move(double offsetX, double offsetY)
+{
+   Point_t offset(offsetX, offsetY);
+   move(offset);
+}
+
+void likhachev::Parallelogram::scale(double multiplier)
+{
+  double width = getWidth();
+  double height = getHeight();
+  Point_t offset(((multiplier * width) - width) / 2, ((multiplier * height) - height) / 2);
+
+  if (pointA_.y == pointB_.y) { // Lavran TODO: Заменить код на слвоари, кода дадут добро. Ну или на множ-ли.
+    bool xMultiplier = 1;
+    bool yMultiplier = 1;
+    if (pointA_.x < pointB_.x) {
+      xMultiplier = 1;
+    }
+    if(pointA_.y < pointD_.y) {
+      yMultiplier = 1;
+    }
+
+    pointA_.x += offset.x * xMultiplier;  // Lavran TODO: Заменить на массив и прогон по циклу, когда дадут добро
+    pointD_.x += offset.x * xMultiplier;
+    pointB_.x -= offset.x * xMultiplier;
+
+    pointA_.y += offset.y * xMultiplier;
+    pointB_.y += offset.y * xMultiplier;
+    pointD_.y -= offset.y * xMultiplier;
+
+  } else {
+    bool xMultiplier = 1;
+    bool yMultiplier = 1;
+    if (pointA_.x < pointB_.x) {
+      xMultiplier = 1;
+    }
+    if(pointA_.y < pointD_.y) {
+      yMultiplier = 1;
+    }
+
+    pointA_.x += offset.x * xMultiplier;  // Lavran TODO: Заменить на массив и прогон по циклу, когда дадут добро
+    pointB_.x += offset.x * xMultiplier;
+    pointD_.x -= offset.x * xMultiplier;
+
+    pointA_.y += offset.y * xMultiplier;
+    pointD_.y += offset.y * xMultiplier;
+    pointB_.y -= offset.y * xMultiplier;
+  }
+}
+
+double likhachev::Parallelogram::getWidth() const
+{
+  Point_t widthPoint;
+  if (pointA_.y == pointB_.y) {
+    widthPoint = pointB_;
+  } else {
+    widthPoint = pointD_;
+  }
+
+  double width = 0;
+  if (pointA_.x > widthPoint.x) {
+    width = pointA_.x - widthPoint.x;
+  } else {
+    width = widthPoint.x - pointA_.x;
+  }
+
+  return width;
+}
+
+double likhachev::Parallelogram::getSubWidth() const
+{
+  Point_t subWidthPoint;
+  if (pointA_.y == pointB_.y) {
+    subWidthPoint = pointD_;
+  } else {
+    subWidthPoint = pointB_;
+  }
+
+  double subWidth = 0;
+  if (pointA_.x > subWidthPoint.x) {
+    subWidth = pointA_.x - subWidthPoint.x;
+  } else {
+    subWidth = subWidthPoint.x - pointA_.x;
+  }
+
+  return subWidth;
+}
+
+double likhachev::Parallelogram::getHeight() const
+{
+  double height = 0;
+  if (pointB_.y > pointD_.y) {
+    height = pointB_.y - pointD_.y;
+  } else {
+    height = pointD_.y - pointB_.y;
+  }
+
+  return height;
+}
---
+++ likhachev.aleksey/T1/parallelogram.hpp
@@ -0,0 +1,27 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+#include "base-types.hpp"
+#include "shape.hpp"
+
+namespace likhachev
+{
+  class Parallelogram: public Shape {
+    public:
+      Parallelogram();
+      Parallelogram(Point_t pointA, Point_t pointB, Point_t pointD);
+      Parallelogram(double pointAX, double pointAY, double pointBX, double pointBY, double pointDX, double pointDY);
+      virtual double getArea() const;
+      virtual likhachev::Rectangle_t getFrameRect() const;
+      virtual void move(Point_t offset);
+      virtual void move(double offsetX, double offsetY);
+      virtual void scale(double multiplier);
+      double getWidth() const;
+      double getSubWidth() const;
+      double getHeight() const;
+    private:
+      Point_t pointA_;
+      Point_t pointB_;
+      Point_t pointD_; 
+  };
+}
+#endif
