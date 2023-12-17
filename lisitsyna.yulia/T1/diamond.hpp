e6ee31ac3c7cbbd5944ac26889e323ada369b7a1
unknown <iulia.lis1tsyna@yandex.ru>
2023-12-17 20:13:17 +0300

    fix

--- lisitsyna.yulia/T1/diamond.hpp
+++ lisitsyna.yulia/T1/diamond.hpp
@@ -4,15 +4,18 @@
 #include "shape.hpp"
 #include "rectangle.hpp"
 
-class Diamond : public Shape
+namespace lisitsyna
 {
-  public:
-    Diamond(point_t one, point_t two, point_t three);
-    Rectangle findLastDiamondPosition(point_t one, point_t two, point_t three);
-    double getArea() const override;
-    rectangle_t getFrameRect() const override;
-    void move(point_t point) override;
-    void move(double delta_x, double delta_y) override;
-    void scale(double k) override;
-};
+  class Diamond : public Shape
+  {
+    public:
+      Diamond(point_t one, point_t two, point_t three);
+      Rectangle DiamondPosition(point_t one, point_t two, point_t three);
+      double getArea() const override;
+      rectangle_t getFrameRect() const override;
+      void move(point_t point) override;
+      void move(double x, double y) override;
+      void scale(double k) override;
+  };
+}
 #endif
