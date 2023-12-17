06dc87835cd59c5053a8e2e20e42fc46674ef1ae
Mark Zheleznyakov <mrqiz@internet.ru>
2023-12-17 23:51:13 +0300

    feat(T1): Реализована функция main с базовыми операциями с Rectangle

--- zheleznyakov.mark/T1/main.cpp
+++ zheleznyakov.mark/T1/main.cpp
@@ -1,4 +1,15 @@
+#include <iostream>
+#include "base-types.hpp"
+#include "rectangle.hpp"
+
 int main()
 {
+  zheleznyakov::point_t leftTopPoint{ 3.0, 4.0 };
+  zheleznyakov::point_t rightBottomPoint{ 7.0, 8.0 };
+  zheleznyakov::Rectangle rectangle(leftTopPoint, rightBottomPoint);
+  std::cout << rectangle.getArea() << '\n';
+  std::cout << rectangle.getFrameRect().width << ' ' << rectangle.getFrameRect().height << '\n';
+  rectangle.move(3, 4);
+  std::cout << rectangle.getFrameRect().pos.x << ' ' << rectangle.getFrameRect().pos.y << '\n';
   return 0;
 }
