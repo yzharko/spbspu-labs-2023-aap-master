30c58f989eb92a275c3d62789c6adacbb8bb9a42
Mark Zheleznyakov <mrqiz@internet.ru>
2023-12-17 23:30:53 +0300

    feat(T1): Добавлена структура rectangle у класса Rectangle, реализован конструктор Rectangle

--- zheleznyakov.mark/T1/rectangle.cpp
+++ zheleznyakov.mark/T1/rectangle.cpp
@@ -1,8 +1,15 @@
 #include "base-types.hpp"
 #include "rectangle.hpp"
 
-zheleznyakov::Rectangle::Rectangle(const point_t leftTopPoint, const point_t RightBottomPoint){
-
+zheleznyakov::Rectangle::Rectangle(const point_t leftTopPoint, const point_t rightBottomPoint){
+  double rectangleWidth = rightBottomPoint.x - leftTopPoint.x;
+  double rectangleHeight = rightBottomPoint.y - leftTopPoint.y;
+  zheleznyakov::point_t currentRectangleCenter;
+  currentRectangleCenter.x = (rectangleWidth) / 2;
+  currentRectangleCenter.y = (rectangleHeight) / 2;
+  rectangle.pos = currentRectangleCenter;
+  rectangle.width = rectangleWidth;
+  rectangle.height = rectangleHeight;
 };
 
 double zheleznyakov::Rectangle::getArea()
--- zheleznyakov.mark/T1/rectangle.hpp
+++ zheleznyakov.mark/T1/rectangle.hpp
@@ -7,12 +7,14 @@ namespace zheleznyakov
   class Rectangle : public zheleznyakov::Shape
   {
   public:
-    Rectangle(const point_t leftTopPoint, const point_t RightBottomPoint);
+    Rectangle(const point_t leftTopPoint, const point_t rightBottomPoint);
     virtual double getArea() override;
     virtual rectangle_t getFrameRect() override;
     virtual void move(double x, double y) override;
     virtual void move(zheleznyakov::point_t point) override;
     virtual void scale(double k) override;
+  private:
+    zheleznyakov::rectangle_t rectangle;
   };
 };
 #endif
