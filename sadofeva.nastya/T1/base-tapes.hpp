aff2b09307e40421a4ee08af33d52f3aaf0ae3d6
sadofeeeva <klover200015@gmail.com>
2023-12-17 22:18:26 +0700

    first

---
+++ sadofeva.nastya/T1/base-tapes.hpp
@@ -0,0 +1,21 @@
+#ifndef BASE_TYPES_HPP
+#define BASE_TYPES_HPP
+
+namespace sadofeva
+{
+	struct Point_t
+	{
+	public:
+		double x;
+		double y;
+	}; 
+	
+	struct Rectangle_t
+	{
+	public:
+		Point_t pos;
+		double width; 
+		double height;
+	};
+}
+#endif
