1fc160b06bb6447552dbe4d6ad9d35ee9a9df3eb
tsomossi <battsolmon.a20sur11a@gmail.com>
2023-12-25 04:58:07 +0300

    ankhbayar.battsolmon/T1

---
+++ ankhbayar.battsolmon/T1/rectangle.hpp
@@ -0,0 +1,24 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+#include "base-types.hpp"
+#include "shape.hpp"
+
+namespace ankhbayar
+{
+  class Rectangle : public Shape
+  {
+  public:
+    Rectangle(point_t bottomLeft, point_t upperRight);
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void moveTo(double x, double y) override;
+    void moveBy(double x, double y) override;
+    void scale(double factor) override;
+  private:
+    point_t center_;
+    double width_;
+    double height_;
+  };
+}
+
+#endif
