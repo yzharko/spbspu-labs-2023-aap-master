2c173d7f1a7b76cbdb16a892dbfdae9d7a0add39
han7ter <sobmisha12@gmail.com>
2023-12-27 17:32:14 +0300

    написал hpp файл для прямоугольника, только хз че в привате писать

--- sobolevsky.misha/T1/rectangle.hpp
+++ sobolevsky.misha/T1/rectangle.hpp
@@ -1,13 +1,20 @@
 #ifndef RECTANGLE_HPP
 #define RECTANGLE_HPP
-#include "base-types.hpp"
+#include "shape.hpp"
 
 namespace sobolevsky
 {
-  class Rectangle
+  class Rectangle : public Shape
   {
   public:
-
+    Rectangle(Point_t leftDownPoint, Point_t rightUpPoint);
+    double getArea();
+    Rectangle_t getFrameRect();
+    void move(Point_t newCenter);
+    void move(double moveX, double moveY);
+    void scale(double n);
+  private:
+    Rectangle_t rectangle;
   };
 }
 
