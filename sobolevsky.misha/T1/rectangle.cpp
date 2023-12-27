c3a0f9d8a17ae85d0855054d22d5ed077de6c547
han7ter <sobmisha12@gmail.com>
2023-12-27 17:52:11 +0300

    написал функцию создания прямоугольника

--- sobolevsky.misha/T1/rectangle.cpp
+++ sobolevsky.misha/T1/rectangle.cpp
@@ -2,8 +2,15 @@
 
 namespace sobolevsky
 {
-  Rectangle(Point_t leftDownPoint, Point_t rightUpPoint)
+  Rectangle::Rectangle(Point_t leftDownPoint, Point_t rightUpPoint)
   {
-    
+    Point_t center;
+    center.x = (leftDownPoint.x - rightUpPoint.x) / 2;
+    center.y = (leftDownPoint.y - rightUpPoint.y) / 2;
+    rectangle.pos = center;
+    rectangle.width = leftDownPoint.x - rightUpPoint.x;
+    rectangle.height = leftDownPoint.y - rightUpPoint.y;
   }
+
+  
 }
