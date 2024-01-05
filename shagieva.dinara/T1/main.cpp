b9b4030fa0c65735b03338db163c49cecde9041c
DINNIESHAY <din200510@gmail.com>
2024-01-05 15:30:23 +0300

    Началась разработка Regular

--- shagieva.dinara/T1/main.cpp
+++ shagieva.dinara/T1/main.cpp
@@ -1,19 +1,28 @@
 #include "base-types.hpp"
 #include "shape.hpp"
 #include "rectangle.hpp"
+#include "complexquad.hpp"
 #include <iostream>
+#include <stdexcept>
 
 int main()
 {
   using namespace shagieva;
-  point_t a = { 2.0, 2.0 };
-  point_t c = { 4.0, 4.0 };
-  Rectangle figure(a, c);
-  std::cout << figure.getArea() << "\n";
-  figure.scale(2);
-  std::cout << figure.getArea() << "\n";
-  point_t newCenter = { 5.0, 5.0 };
-  figure.move(newCenter);
-  rectangle_t frame = figure.getFrameRect();
+
+  point_t pointA = { 1.0, 2.0 };
+  point_t pointB = { 9.0, 2.0 };
+  point_t pointC = { 13.0, 7.0 };
+  point_t pointD = { 3.0, 6.0 };
+
+  Complexquad figure(pointA, pointB, pointC, pointD);
+  try
+  {
+    std::cout << figure.getArea() << "\n";
+  }
+  catch (const std::invalid_argument & e)
+  {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
   return 0;
 }
