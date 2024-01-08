2c562889d1cbf638b18ead5119e2359ebadaf6a9
pinniks <nik.nwlife@gmail.com>
2024-01-08 20:26:36 +0300

    concave has been added

--- popov.nikita/T1/regular.cpp
+++ popov.nikita/T1/regular.cpp
@@ -7,7 +7,7 @@ popov::Regular::Regular(float x1, float y1, float x2, float y2,float x3, float y
   third_pos_{x3,y3};
 float popov::Regular::getDistance(const point_t& other)
 {
-  return sqrt(abs(pow((x_ - other.x_), 2) + pow((y_ - other.y_), 2)));
+  return sqrt(abs(pow((x - other.x), 2) + pow((y - other.y), 2)));
 }
 float popov::Regular::getArea()
 {
@@ -19,19 +19,19 @@ popov::rectangle_t popov::Rectangle::getFrameRect()
 {
   float width = first_pos_.getDistance(second_pos_) * 2;
   float height = first_pos_.getDistance(second_pos_) * 2;
-  point_t pos.x = first_pos_.x - width / 2);
-  point_t pos.y = first_pos_.y - height / 2);
+  point_t pos.x = (first_pos_.x - width) / 2;
+  point_t pos.y = (first_pos_.y - height) / 2;
   return {width, hight, pos};
 }
 void popov::Regular::move(float x, float y)
 {
-  first_pos_{x1 + x,y1 + y},
-  second_pos_{x2 + x,y2 + y},
-  third_pos_{x3 + x,y3 + y};
+  first_pos_{first_pos_.x + x,first_pos_.y + y},
+  second_pos_{second_pos_.x + x,second_pos_.y + y},
+  third_pos_{third_pos_.x + x,third_pos_.y + y};
 }
-void scale(float n)
+void popov::Regular::scale(float n)
 {
-  first_pos_{x1 * n,y1 * n},
-  second_pos_{x2 * n,y2 * n},
-  third_pos_{x3 * n,y3 * n};
+  first_pos_{first_pos_.x * n,first_pos_.y * n},
+  second_pos_{second_pos_.x * n,second_pos_.y * n},
+  third_pos_{third_pos_.x * n,third_pos_.y * n};
 }
