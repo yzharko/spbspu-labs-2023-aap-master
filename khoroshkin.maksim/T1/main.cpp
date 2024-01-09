648428306c8e97871005c96baf9dd533cf92c26b
Maxim Khoroshkin <khoroskinmaks@gmail.com>
2024-01-09 17:48:47 +0300

    doing main.cpp

--- khoroshkin.maksim/T1/main.cpp
+++ khoroshkin.maksim/T1/main.cpp
@@ -1,48 +1,96 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include "base-types.hpp"
+#include "shape.hpp"
 #include "rectangle.hpp"
 #include "circle.hpp"
 #include "complexquad.hpp"
 
 int main()
 {
-  std::string inputLine;
-  while (std::getline(std::cin, inputLine))
+  size_t initialSize = 10;
+  Shape ** allFigures = new Shape * [initialSize];
+  size_t figuresCounter = 0;
+  std::string type;
+  while (std::cin >> type)
   {
-    if (std::cin.eof())
+    if (figuresCounter == initialSize)
     {
-      return 1;
+      initialSize += initialSize;
+      Shape ** tempStorage = new Shape * [initialSize];
+      std::copy(allFigures, allFigures + figuresCounter, tempStorage);
+      delete[] allFigures;
+      allFigures = tempStorage;
     }
-    std::string type;
-    std::istringstream input(inputLine);
-    input >> type;
+
     if (type == "RECTANGLE")
     {
-      double leftLow1, leftLow2, rightHigh1, rightHigh2;
-      input >> leftLow1 >> leftLow2 >> rightHigh1 >> rightHigh2;
-
+      double x1, y1, x2, y2;
+      if (std::cin >> x1 >> y1 >> x2 >> y2)
+      {
+        allFigures[figuresCounter++] = new khoroshkin::Rectangle(point_t{x1, y1}, point_t{x2, y2});
+      }
+      else
+      {
+        std::cerr << "Error: wrong rectangle input\n";
+      }
     }
     else if (type == "CIRCLE")
     {
-      double center1, center2, radius;
-      if (radius <= 0)
+      double x, y, radius;
+      if (std::cin >> x >> y >> radius && radius > 0)
       {
-
+        allFigures[figuresCounter++] = new khoroshkin::Circle(point_t{x, y}, radius);
       }
-      input >> center1 >> center2 >> radius;
-
+      else
+      {
+        std::cerr << "Error: wrong circle input\n";
+      }
     }
     else if (type == "COMPLEXQUAD")
     {
-      double firstSide1, firstSide2, secondSide1, secondSide2;
-      input >> firstSide1 >> firstSide2 >> secondSide1 >> secondSide2;
+      double x1, y1, x2, y2, x3, y3, x4, y4;
+      if (std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4)
+      {
+        allFigures[figuresCounter++] = new khoroshkin::Complexquad(point_t{x1, y1}, point_t{x2, y2},\
+        point_t{x3, y3}, point_t{x4, y4});
+      }
+      else
+      {
+        std::cerr << "Error: wrong complexquad input\n";
+      }
+    }
+    else if (type == "SCALE")
+    {
+      double x, y, scale;
+      if (std::cin >> x >> y >> scale && scale > 0)
+      {
+        for (size_t i = 0; i < figuresCounter; ++i)
+        {
+          allFigures[i]->scale(scale);
+        }
 
+        for (size_t i = 0; i < figuresCounter; ++i)
+        {
+          std::cout << allFigures[i]->getArea() << " " << allFigures[i]->getFrameRect().pos.x << " " \
+                    << allFigures[i]->getFrameRect().pos.y << " " << allFigures[i]->getFrameRect().width << " " \
+                    << allFigures[i]->getFrameRect().height << "\n";
+        }
+      }
+      else
+      {
+        std::cerr << "Error: wrong scale input\n";
+      }
     }
     else
     {
-      std::cout << inputLine;
+      std::cerr << "Error: unsupported figure\n";
     }
+  }
+  for (size_t i = 0; i < figuresCounter; ++i)
+  {
+    delete[] allFigures[i];
   }
   return 0;
 }
