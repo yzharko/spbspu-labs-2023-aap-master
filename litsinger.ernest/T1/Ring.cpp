37695f1bd0d4806cb58d03ada619a53121d80918
0412erik <litsinger00@bk.ru>
2024-01-09 20:42:53 +0300

    fix

--- litsinger.ernest/T1/Ring.cpp
+++ litsinger.ernest/T1/Ring.cpp
@@ -2,70 +2,74 @@
 
 Ring::Ring(double xCenter, double yCenter, double outSideRadius, double innerRadius) :
 	center{ xCenter,yCenter } {
-	if (outSideRadius < innerRadius || outSideRadius <= 0) {
-		throw std::invalid_argument("outside R error\n");
+  if (outSideRadius < innerRadius || outSideRadius <= 0) {
+    throw std::invalid_argument("outside R error\n");
 	}
-	externalRadius = outSideRadius;
-	if (innerRadius <= 0) {
-		throw std::invalid_argument("inner R error\n");
-	}
-	this->innerRadius = innerRadius;
+  externalRadius = outSideRadius;
+  if (innerRadius <= 0) {
+    throw std::invalid_argument("inner R error\n");
+  }
+  this->innerRadius = innerRadius;
 }
 
 double Ring::getArea() const {
 #define pi 3.14
-	double square = pi * (externalRadius * externalRadius - innerRadius * innerRadius);
-	return square;
+  double square = pi * (externalRadius * externalRadius - innerRadius * innerRadius);
+  return square;
 }
 
 base_types::rectangle_t Ring::getFrameRect() const {
-	return base_types::rectangle_t{ 2 * externalRadius, 2 * externalRadius, center };
+  return base_types::rectangle_t{ 2 * externalRadius, 2 * externalRadius, center };
 }
 
 void Ring::move(double dx, double dy) {
-	center.x += dx;
-	center.y += dy;
+  center.x += dx;
+  center.y += dy;
 }
 
 void Ring::move(const base_types::point_t& point) {
-	center = point;
+  center = point;
 }
 
 void Ring::scale(double k) {
-	if (k <= 0) {
-		throw std::invalid_argument("incorrect k");
-	}
-	externalRadius *= k;
-	innerRadius *= k;
+	if (k <= 0) 
+  {
+    throw std::invalid_argument("incorrect k");
+  }
+  externalRadius *= k;
+  innerRadius *= k;
 }
 
 bool Ring::operator==(const Shape& other) const {
 	const Ring* otherRing = dynamic_cast<const Ring*>(&other);
-	if (otherRing) {
-		if (this == otherRing) {
-			return true;
-		}
-		if (this->center == otherRing->center &&
+	if (otherRing) 
+  {
+    if (this == otherRing) 
+    {
+      return true;
+    }
+    if (this->center == otherRing->center &&
 			this->externalRadius == otherRing->externalRadius &&
-			this->innerRadius == otherRing->innerRadius) {
-			return true;
-		}
-	}
-	return false;
+			this->innerRadius == otherRing->innerRadius) 
+    {
+      return true;
+    }
+  }
+  return false;
 }
 
 double Ring::getExternalRadius() const {
-	return externalRadius;
+  return externalRadius;
 }
 
 double Ring::getInnerRadius() const {
-	return innerRadius;
+  return innerRadius;
 }
 
 const base_types::point_t& Ring::getCenter() const {
-	return center;
+  return center;
 }
 
 std::ostream& operator<<(std::ostream& os, const Ring& ring) {
-	return os << "Ring:\n external R = " << ring.getExternalRadius() << "\n inner R = " << ring.getInnerRadius() << "\n center = " << ring.getCenter();
+  return os << "Ring:\n external R = " << ring.getExternalRadius() << "\n inner R = " << ring.getInnerRadius() << "\n center = " << ring.getCenter();
 }
