95dee843cacf46ba88ff505203232d44a4eed793
0412erik <litsinger00@bk.ru>
2024-01-09 21:08:52 +0300

    fix cg 1 11

--- litsinger.ernest/T1/Shape.hpp
+++ litsinger.ernest/T1/Shape.hpp
@@ -1,9 +1,9 @@
 #include"BaseTypes.hpp"
 class Shape {
 public:
-	virtual double getArea() const = 0;
-	virtual base_types::rectangle_t getFrameRect() const = 0;
-	virtual void move(double x, double y) = 0;
-	virtual void move(const base_types::point_t& point) = 0;
-	virtual void scale(double k) = 0;
+  virtual double getArea() const = 0;
+  virtual base_types::rectangle_t getFrameRect() const = 0;
+  virtual void move(double x, double y) = 0;
+  virtual void move(const base_types::point_t& point) = 0;
+  virtual void scale(double k) = 0;
 };
