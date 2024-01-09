1d636b8ee901a15067d6281418ebf3b763fe2089
amotvei <motya2228@mail.ru>
2024-01-09 22:29:21 +0300

    fixed errors of CG

--- taskaev.matvei/T1/Shape.hpp
+++ taskaev.matvei/T1/Shape.hpp
@@ -8,7 +8,7 @@ public:
   virtual double getArea() = 0;
   virtual rectangle_t getFrameRect() = 0;
   virtual void move(double x, double y) = 0;
-  virtual void move(const point_t &center) = 0;
+  virtual void move(point_t newCenter) = 0;
   virtual void scale(double k) = 0;
 };
 #endif
--- taskaev.matvei/T1/rectangle.cpp
+++ taskaev.matvei/T1/rectangle.cpp
@@ -24,10 +24,10 @@ void taskaev::Rectangle::move(double x, double y)
   leftPoint_ = point_t(leftPoint_.X + x, leftPoint_.Y + y);
 }
 
-void taskaev::Rectangle::move(point_t center)
+void taskaev::Rectangle::move(point_t newCenter)
 {
-  double d_x = (center.X - ((rightPoint_.X + leftPoint_.X) / 2));
-  double d_y = (center.Y - ((rightPoint_.Y + leftPoint_.Y) / 2));
+  double d_x = (newCenter.X - ((rightPoint_.X + leftPoint_.X) / 2));
+  double d_y = (newCenter.Y - ((rightPoint_.Y + leftPoint_.Y) / 2));
   move(d_x, d_y);
 
 }
