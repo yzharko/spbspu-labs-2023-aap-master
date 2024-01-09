50d92bd6eb43e03e1985c2ee65f211b7541934ee
0412erik <litsinger00@bk.ru>
2024-01-09 22:07:26 +0300

    fix cg try

--- litsinger.ernest/T1/BaseTypes.hpp
+++ litsinger.ernest/T1/BaseTypes.hpp
@@ -1,21 +1,19 @@
 #ifndef BASETYPES_HPP
 #define BASETYPES_HPP
 #include<iostream>
-namespace base_types
+
+struct point_t
 {
-  struct point_t
-  {
-    double x;
-    double y;
-  };
-  struct rectangle_t
-  {
-    double width;
-	  double height;
-    point_t pos;
-  };
-  bool operator==(const point_t& lhs, const point_t& rhs);
-  std::ostream& operator << (std::ostream& os, const base_types::point_t& point);
-  std::ostream& operator << (std::ostream& os, const base_types::rectangle_t& rectangle);
-}
+  double x;
+  double y;
+};
+struct rectangle_t
+{
+  double width;
+	double height;
+  point_t pos;
+};
+bool operator==(const point_t& lhs, const point_t& rhs);
+std::ostream& operator << (std::ostream& os, const base_types::point_t& point);
+std::ostream& operator << (std::ostream& os, const base_types::rectangle_t& rectangle);
 #endif
