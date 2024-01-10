6d6a737e2770fb3a284917367fed102e4e4826ef
han7ter <sobmisha12@gmail.com>
2024-01-11 01:59:14 +0300

    начал делать параллелограм

--- sobolevsky.misha/T1/parallelogram.hpp
+++ sobolevsky.misha/T1/parallelogram.hpp
@@ -4,10 +4,20 @@
 
 namespace sobolevsky
 {
-  class Parallelogram
+  class Parallelogram : public Shape
   {
-  public:
-
+    Parallelogram(Point_t Point1_1, Point_t Point2_1, Point_t Point3_1);
+    double getArea();
+    Rectangle_t getFrameRect();
+    void move(Point_t newCenter);
+    void move(double moveX, double moveY);
+    void scale(double n);
+  private:
+    Rectangle_t rectangle;
+    Point_t Point1;
+    Point_t Point2;
+    Point_t Point3;
+    Point_t centerPoint;
   };
 }
 
