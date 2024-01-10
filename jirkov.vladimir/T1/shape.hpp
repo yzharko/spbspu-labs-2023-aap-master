fd7b2d116d4a7f2008b58f98d01d33825a090d4a
jirkov17 <JIRKOFF17@GMAIL.COM>
2024-01-10 03:57:54 +0300

    fix shape.hpp

--- jirkov.vladimir/T1/shape.hpp
+++ jirkov.vladimir/T1/shape.hpp
@@ -1,8 +1,9 @@
 #ifndef SHAPE_HPP
 #define SHAPE_HPP
-
+#include "base-types.hpp"
 class Shape
 {
+public:
   virtual double getArea() = 0;
   virtual rectangle_t getFrameRect() = 0;
   virtual void move(double x, double y) = 0;
