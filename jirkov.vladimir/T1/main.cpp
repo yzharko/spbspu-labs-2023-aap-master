1ac03344fa41a2b90bbb84e16426de277ed390af
jirkov17 <JIRKOFF17@GMAIL.COM>
2024-01-10 04:11:08 +0300

    add main.cpp

---
+++ jirkov.vladimir/T1/main.cpp
@@ -0,0 +1,22 @@
+#include "shape.hpp"
+#include "rectangle.hpp"
+#include "Diamond.hpp"
+#include <iostream>
+
+int main()
+{
+  Rectangle rectangle({1.0, 4.0}, {9.0, 4.0});
+  std::cout << rectangle.getArea() << "\n";
+  rectangle_t frameRectangle = rectangle.getFrameRect();
+  rectangle.move({0.0, 0.0});
+  rectangle.move(1.0, -1.0);
+  rectangle.scale(3.5);
+
+  int num = 5;
+//  points[0] = {5.0, 9.0};
+//  points[1] = {12.0, 5.0};
+//  points[2] = {8.0, 2.0};
+//  points[3] = {4.0, 3.0};
+//  points[4] = {3.0, 6.0};
+
+}
