3db8d86e405f62b951491a8998ff69dd274d82f8
han7ter <sobmisha12@gmail.com>
2024-01-11 02:02:42 +0300

    начал писать cpp файл для параллелограма

---
+++ sobolevsky.misha/T1/parallelogram.cpp
@@ -0,0 +1,34 @@
+#include "parallelogram.hpp"
+
+namespace sobolevsky
+{
+  Parallelogram::Parallelogram(Point_t Point1_1, Point_t Point2_1, Point_t Point3_1)
+  {
+
+  }
+
+  double Parallelogram::getArea()
+  {
+
+  }
+
+  Rectangle_t Parallelogram::getFrameRect()
+  {
+
+  }
+  
+  void Parallelogram::move(Point_t newCenter)
+  {
+
+  }
+  
+  void Parallelogram::move(double moveX, double moveY)
+  {
+
+  }
+  
+  void Parallelogram::scale(double n)
+  {
+
+  }
+}
