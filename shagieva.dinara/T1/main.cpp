666c37b5798096398f492466a8695cf668369904
DINNIESHAY <din200510@gmail.com>
2024-01-11 02:13:18 +0300

    Исправлено масштабирование

--- shagieva.dinara/T1/main.cpp
+++ shagieva.dinara/T1/main.cpp
@@ -42,16 +42,23 @@ int main()
       break;
     }
 
-    double x1, y1, x2, y2, x3, y3, x4, y4;
-
     if (figureType == "RECTANGLE")
     {
+      double x1, y1, x2, y2;
       std::cin >> x1 >> y1 >> x2 >> y2;
-      shapes[shapeCount++] = new shagieva::Rectangle(shagieva::point_t{ x1, y1 }, shagieva::point_t{ x2, y2 });
+      if (x1 < x2 && y1 < y2)
+      {
+        shapes[shapeCount++] = new shagieva::Rectangle(shagieva::point_t{ x1, y1 }, shagieva::point_t{ x2, y2 });
+      }
+      else
+      {
+        figureIsWrong = true;
+      }
     }
 
     else if (figureType == "COMPLEXQUAD")
     {
+      double x1, y1, x2, y2, x3, y3, x4, y4;
       std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
       shapes[shapeCount++] = new shagieva::Complexquad(shagieva::point_t{ x1, y1 },
         shagieva::point_t{ x2, y2 },
@@ -61,6 +68,7 @@ int main()
 
     else if (figureType == "REGULAR")
     {
+      double x1, y1, x2, y2, x3, y3;
       std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
       shapes[shapeCount++] = new shagieva::Regular(shagieva::point_t{ x1, y1 }, shagieva::point_t{ x2, y2 },
         shagieva::point_t{ x3, y3 });
@@ -113,9 +121,13 @@ int main()
 
   for (size_t i = 0; i < shapeCount; ++i)
   {
-    shagieva::Shape * shape = shapes[i];
-    shape->move({ scaleCenterX, scaleCenterY });
-    shape->scale(scaleFactor);
+    shagieva::rectangle_t frameBeforeMoving = shapes[i]->getFrameRect();
+    shapes[i]->move({ scaleCenterX, scaleCenterY });
+    shagieva::rectangle_t frameAfterMoving = shapes[i]->getFrameRect();
+    shapes[i]->scale(scaleFactor);
+    double dx = (frameBeforeMoving.pos.x - frameAfterMoving.pos.x) * scaleFactor;
+    double dy = (frameBeforeMoving.pos.y - frameAfterMoving.pos.y) * scaleFactor;
+    shapes[i]->move(dx, dy);
   }
 
   double totalAreaAfterScaling = 0.0;
