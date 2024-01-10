ed1decc3b5fafdf8dbde1f02e46e0ee0b9771b71
han7ter <sobmisha12@gmail.com>
2024-01-11 02:07:55 +0300

    написал метод создания паларелограмма

--- sobolevsky.misha/T1/parallelogram.cpp
+++ sobolevsky.misha/T1/parallelogram.cpp
@@ -4,7 +4,11 @@ namespace sobolevsky
 {
   Parallelogram::Parallelogram(Point_t Point1_1, Point_t Point2_1, Point_t Point3_1)
   {
-
+    Point1 = Point1_1;
+    Point2 = Point2_1;
+    Point3 = Point3_1;
+    centerPoint.x = (Point2.x + Point3.x) / 2;
+    centerPoint.y = (Point2.y + Point3.y) / 2;
   }
 
   double Parallelogram::getArea()
