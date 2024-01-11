1c47711213d5dee3fa3322b87d403cfea0915dda
shavel0k <anna.reznikova@icloud.com>
2024-01-11 22:15:52 +0300

    try to fix acceptance

--- reznikova.anna/T1/main.cpp
+++ reznikova.anna/T1/main.cpp
@@ -94,13 +94,13 @@ int main()
       {
         sum_area += figures[i]->getArea();
       }
-      std::cout << std::fixed << std::setprecision(1) << sum_area << " \0";
+      std::cout << std::fixed << std::setprecision(1) << sum_area << "\0 ";
 
       for (int i = 0; i < stored; i++)
       {
         rectangle_t frame_rect = figures[i]->getFrameRect();
-        std::cout << std::fixed << std::setprecision(1) << frame_rect.leftpoint.x << "\0 " << frame_rect.leftpoint.y << \0" ";
-        std::cout << std::fixed << std::setprecision(1) << frame_rect.rightpoint.x << \0" " << frame_rect.rightpoint.y << "\0";
+        std::cout << std::fixed << std::setprecision(1) << frame_rect.leftpoint.x << "\0 " << frame_rect.leftpoint.y << "\0 ";
+        std::cout << std::fixed << std::setprecision(1) << frame_rect.rightpoint.x << "\0 " << frame_rect.rightpoint.y << "\0";
       }
       std::cout << "\n";
 
