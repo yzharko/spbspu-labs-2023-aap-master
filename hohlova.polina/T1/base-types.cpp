bb3d11c2956d35bc118fe343c18d23d3e88e311b
phohlova <pv.hohlova@edu.spbstu.ru>
2024-01-11 16:57:41 +0300

    -

---
+++ hohlova.polina/T1/base-types.cpp
@@ -0,0 +1,12 @@
+#include "base-types.hpp"
+
+point_t::point_t(double x, double y) :
+  x_(x),
+  y_(y)
+{}
+
+rectangle_t::rectangle_t(double width, double height, point_t pos):
+  width_(width),
+  height_(height),
+  pos_(pos)
+{}
