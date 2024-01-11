35d230a7d44b43e1ce5c09ca2e3aee97a8e7e0c9
oksidmargantsa <sukachva.maria@mail.ru>
2024-01-12 00:16:58 +0300

    work on main

--- sukacheva.maria/T1/main.cpp
+++ sukacheva.maria/T1/main.cpp
@@ -69,49 +69,56 @@ int main() {
     else if (nameOfFigure == "SCALE")
     {
       double x, y, k = 0;
-      double x1, y1, x2, y2 = 0;
-      double x3, y3, x4, y4 = 0;
-      std::cin >> x >> y >> k;
-      point_t center{ x, y };
-      double firstArea = 0;
-      double newArea = 0;
-      for (int i = 0; i < index; i++)
+      if ( k < 0 || index == 0)
       {
-        firstArea += figures[i]->getArea();
+        std::cerr << "Incorrect input of figures";
+        return 2;
       }
-      std::cout << std::fixed << std::setprecision(1) << firstArea;
+      else {
+        double x1, y1, x2, y2 = 0;
+        double x3, y3, x4, y4 = 0;
+        std::cin >> x >> y >> k;
+        point_t center{ x, y };
+        double firstArea = 0;
+        double newArea = 0;
+        for (int i = 0; i < index; i++)
+        {
+          firstArea += figures[i]->getArea();
+        }
+        std::cout << std::fixed << std::setprecision(1) << firstArea;
 
-      for (int i = 0; i < index; i++)
-      {
-        rectangle_t frameRect = figures[i]->getFrameRect();
-        x1 = frameRect.pos_.x_ - frameRect.width_ / 2.0;
-        y1 = frameRect.pos_.y_ - frameRect.height_ / 2.0;
-        x2 = frameRect.pos_.x_ + frameRect.width_ / 2.0;
-        y2 = frameRect.pos_.y_ + frameRect.height_ / 2.0;
-        std::cout << std::fixed << std::setprecision(1) << ' ' << x1 << ' ' << y1 << ' ' << x2 << ' ' << y2;
+        for (int i = 0; i < index; i++)
+        {
+          rectangle_t frameRect = figures[i]->getFrameRect();
+          x1 = frameRect.pos_.x_ - frameRect.width_ / 2.0;
+          y1 = frameRect.pos_.y_ - frameRect.height_ / 2.0;
+          x2 = frameRect.pos_.x_ + frameRect.width_ / 2.0;
+          y2 = frameRect.pos_.y_ + frameRect.height_ / 2.0;
+          std::cout << std::fixed << std::setprecision(1) << ' ' << x1 << ' ' << y1 << ' ' << x2 << ' ' << y2;
+        }
+
+        std::cout << "\n";
+
+        for (int i = 0; i < index; i++)
+        {
+          figures[i]->scale(center, k);
+          newArea += figures[i]->getArea();
+        }
+        std::cout << std::fixed << std::setprecision(1) << newArea;
+
+        for (int i = 0; i < index; i++)
+        {
+          rectangle_t newFrameRect = figures[i]->getFrameRect();
+          x3 = newFrameRect.pos_.x_ - newFrameRect.width_ / 2.0;
+          y3 = newFrameRect.pos_.y_ - newFrameRect.height_ / 2.0;
+          x4 = newFrameRect.pos_.x_ + newFrameRect.width_ / 2.0;
+          y4 = newFrameRect.pos_.y_ + newFrameRect.height_ / 2.0;
+          std::cout << std::fixed << std::setprecision(1) << ' ' << x3 << ' ' << y3 << ' ' << x4 << ' ' << y4;
+          delete figures[i];
+        }
+        std::cout << "\n";
+        break;
       }
-
-      std::cout << "\n";
-
-      for (int i = 0; i < index; i++)
-      {
-        figures[i]->scale(center, k);
-        newArea += figures[i]->getArea();
-      }
-      std::cout << std::fixed << std::setprecision(1) << newArea;
-
-      for (int i = 0; i < index; i++)
-      {
-        rectangle_t newFrameRect = figures[i]->getFrameRect();
-        x3 = newFrameRect.pos_.x_ - newFrameRect.width_ / 2.0;
-        y3 = newFrameRect.pos_.y_ - newFrameRect.height_ / 2.0;
-        x4 = newFrameRect.pos_.x_ + newFrameRect.width_ / 2.0;
-        y4 = newFrameRect.pos_.y_ + newFrameRect.height_/ 2.0;
-        std::cout << std::fixed << std::setprecision(1) << ' '  << x3 << ' ' << y3 << ' ' << x4 << ' ' << y4;
-        delete figures[i];
-      }
-      std::cout << "\n";
-      break;
     }
   };
   return 0;
