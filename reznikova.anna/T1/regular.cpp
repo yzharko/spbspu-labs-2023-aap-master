002203f39986279f0b9685fb8f9afa32c04fdcdd
shavel0k <anna.reznikova@icloud.com>
2024-01-12 05:47:02 +0300

    ахах у прошлого комита очень смешное название. простите, я уже сошла с ума

--- reznikova.anna/T1/regular.cpp
+++ reznikova.anna/T1/regular.cpp
@@ -12,8 +12,7 @@ reznikova::Regular::Regular(point_t center, point_t point1, point_t point2):
   r = (side1 < side2 ? side1 : side2);
   R = (side1 > side2 ? side1 : side2);
   num_sides = M_PI / (acos(r/R));
-  double epsilon = 0.00000000000001;
-  if (abs(num_sides - floor(num_sides + 0.5)) > epsilon)
+  if ((M_PI / (acos(r/R))) - floor(M_PI / (acos(r/R))) != 0)
   {
     throw std::runtime_error("wrong parameters\n");
   }
