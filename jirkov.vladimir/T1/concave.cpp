6986a07814b4b39ed6a8673c356af62781295e74
jirkov17 <JIRKOFF17@GMAIL.COM>
2024-01-13 02:37:30 +0300

    fixed new concave

--- jirkov.vladimir/T1/concave.cpp
+++ jirkov.vladimir/T1/concave.cpp
@@ -10,82 +10,24 @@ Concave::Concave(point_t firstPoint, point_t secondPoint, point_t thirdPoint, po
   fourthPoint_(fourthPoint)
 {}
 
-double Concave::getArea() const
-{
-  double firstDiffX = firstPoint_.x - secondPoint_.x;
-  double firstDiffY = firstPoint_.y - secondPoint_.y;
-  double secondDiffX = secondPoint_.x - thirdPoint_.x;
-  double secondDiffY = secondPoint_.y - thirdPoint_.y;
-  double thirdDiffX = thirdPoint_.x - firstPoint_.x;
-  double thirdDiffY = thirdPoint_.y - firstPoint_.y;
+double Concave::getArea() const {
+  double x1 = firstPoint_.x;
+  double y1 = firstPoint_.y;
+  double x2 = secondPoint_.x;
+  double y2 = secondPoint_.y;
+  double x3 = thirdPoint_.x;
+  double y3 = thirdPoint_.y;
+  double x4 = fourthPoint_.x;
+  double y4 = fourthPoint_.y;
 
-  double firstSqrt = sqrt(firstDiffX * firstDiffX + firstDiffY * firstDiffY);
-  double secondSqrt = sqrt(secondDiffX * secondDiffX + secondDiffY * secondDiffY);
-  double thirdSqrt = sqrt(thirdDiffX * thirdDiffX + thirdDiffY * thirdDiffY);
-  double mediumSqrt = (firstSqrt + secondSqrt + thirdSqrt) * 0.5;
-  double firstSquare = sqrt(mediumSqrt * (mediumSqrt - firstSqrt) * (mediumSqrt - secondSqrt) * (mediumSqrt - thirdSqrt));
+  double firstArea = (x1 * (y2 - y3)) + (x2 * (y3 - y1)) + (x3 * (y1 - y2));
+  firstArea = std::abs(firstArea / 2.0);
 
-  double maxSide = (firstSqrt >= secondSqrt) ? firstSqrt : secondSqrt;
-  maxSide = (maxSide >= thirdSqrt) ? maxSide : thirdSqrt;
+  double secondArea = (x4 * (y2 - y3)) + (x2 * (y3 - y4)) + (x3 * (y4 - y2));
+  secondArea = std::abs(secondArea / 2.0);
 
-  if (maxSide >= firstSqrt + secondSqrt + thirdSqrt - maxSide)
-  {
-    throw std::logic_error("Triangle error");
-  }
-
-  double fourthDiffX = fourthPoint_.x - secondPoint_.x;
-  double fourthDiffY = fourthPoint_.y - secondPoint_.y;
-  double fifthDiffX = secondPoint_.x - thirdPoint_.x;
-  double fifthDiffY = secondPoint_.y - thirdPoint_.y;
-  double sixthDiffX = thirdPoint_.x - fourthPoint_.x;
-  double sixthDiffY = thirdPoint_.y - fourthPoint_.y;
-  double fourthSqrt = sqrt(fourthDiffX * fourthDiffX + fourthDiffY * fourthDiffY);
-  double fifthSqrt = sqrt(fifthDiffX * fifthDiffX + fifthDiffY * fifthDiffY);
-  double sixthSqrt = sqrt(sixthDiffX * sixthDiffX + sixthDiffY * sixthDiffY);
-  double seventhSqrt = (fourthSqrt + fifthSqrt + sixthSqrt) * 0.5;
-  double secondSquare = sqrt(seventhSqrt * (seventhSqrt - fourthSqrt) * (seventhSqrt - fifthSqrt) * (seventhSqrt - sixthSqrt));
-
-  maxSide = (fourthSqrt >= fifthSqrt) ? fourthSqrt : fifthSqrt;
-  maxSide = (maxSide >= sixthSqrt) ? maxSide : sixthSqrt;
-
-  if (maxSide >= fourthSqrt + fifthSqrt + sixthSqrt - maxSide)
-  {
-    throw std::logic_error("Triangle error");
-  }
-
-  double eighthDiffX = firstPoint_.x - secondPoint_.x;
-  double eighthDiffY = firstPoint_.y - secondPoint_.y;
-  double ninthDiffX = secondPoint_.x - fourthPoint_.x;
-  double ninthDiffY = secondPoint_.y - fourthPoint_.y;
-  double tenthDiffX = fourthPoint_.x - firstPoint_.x;
-  double tenthDiffY = fourthPoint_.y - firstPoint_.y;
-
-  maxSide = (firstSqrt >= secondSqrt) ? firstSqrt : secondSqrt;
-  maxSide = (maxSide >= thirdSqrt) ? maxSide : thirdSqrt;
-
-  if (maxSide >= firstSqrt + secondSqrt + thirdSqrt - maxSide)
-  {
-    throw std::logic_error("Triangle error");
-  }
-
-  eighthDiffX = firstPoint_.x - thirdPoint_.x;
-  eighthDiffY = firstPoint_.y - thirdPoint_.y;
-  ninthDiffX = thirdPoint_.x - fourthPoint_.x;
-  ninthDiffY = thirdPoint_.y - fourthPoint_.y;
-  tenthDiffX = fourthPoint_.x - firstPoint_.x;
-  tenthDiffY = fourthPoint_.y - firstPoint_.y;
-
-  maxSide = (firstSqrt >= secondSqrt) ? firstSqrt : secondSqrt;
-  maxSide = (maxSide >= thirdSqrt) ? maxSide : thirdSqrt;
-
-  if (maxSide >= firstSqrt + secondSqrt + thirdSqrt - maxSide)
-  {
-    throw std::logic_error("Triangle error");
-  }
-
-  return firstSquare - secondSquare;
+  return std::abs(firstArea - secondArea);
 }
-
 rectangle_t Concave::getFrameRect() const
 {
   double maxX = std::max({ firstPoint_.x, secondPoint_.x, thirdPoint_.x });
@@ -99,13 +41,14 @@ rectangle_t Concave::getFrameRect() const
   point_t pos = { posX, posY };
   return rectangle_t{ width, height, pos };
 }
-void Concave::move(const point_t& destination)
+
+void Concave::move(const point_t & destination)
 {
   double moveX = destination.x - fourthPoint_.x;
   double moveY = destination.y - fourthPoint_.y;
-
   move(moveX, moveY);
 }
+
 void Concave::move(double moveX, double moveY)
 {
   firstPoint_.x += moveX;
@@ -120,13 +63,20 @@ void Concave::move(double moveX, double moveY)
 
 void Concave::scale(double k)
 {
-  double scalingFactor = k - 1;
   point_t pos = fourthPoint_;
 
-  firstPoint_.x += (firstPoint_.x - pos.x) * scalingFactor;
-  firstPoint_.y += (firstPoint_.y - pos.y) * scalingFactor;
-  secondPoint_.x += (secondPoint_.x - pos.x) * scalingFactor;
-  secondPoint_.y += (secondPoint_.y - pos.y) * scalingFactor;
-  thirdPoint_.x += (thirdPoint_.x - pos.x) * scalingFactor;
-  thirdPoint_.y += (thirdPoint_.y - pos.y) * scalingFactor;
+  double dx = (firstPoint_.x - pos.x) * (k - 1);
+  double dy = (firstPoint_.y - pos.y) * (k - 1);
+  firstPoint_.x += dx;
+  firstPoint_.y += dy;
+
+  dx = (secondPoint_.x - pos.x) * (k - 1);
+  dy = (secondPoint_.y - pos.y) * (k - 1);
+  secondPoint_.x += dx;
+  secondPoint_.y += dy;
+
+  dx = (thirdPoint_.x - pos.x) * (k - 1);
+  dy = (thirdPoint_.y - pos.y) * (k - 1);
+  thirdPoint_.x += dx;
+  thirdPoint_.y += dy;
 }
