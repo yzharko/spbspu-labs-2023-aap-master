dd82df6c2ab9358671871e7ad0000f02c710d29e
allleng05 <allleng04@mail.ru>
2024-01-12 16:06:08 +0300

    -

--- gorbunova.alena/T1/shape.hpp
+++ gorbunova.alena/T1/shape.hpp
@@ -7,7 +7,7 @@ namespace gorbunova
   class Shape
   {
   public:
-    virtual ~Shape() {}
+    virtual ~Shape() = default;
     virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
     virtual void move(point_t center) = 0;
