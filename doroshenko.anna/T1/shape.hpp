71aa7dd0696d91666f2dfaf377862a43a206a43e
anyadg <anna_giraffe@mail.ru>
2024-01-12 20:02:35 +0300

    Added const

--- doroshenko.anna/T1/shape.hpp
+++ doroshenko.anna/T1/shape.hpp
@@ -10,7 +10,7 @@ namespace doroshenko
     virtual ~Shape() = default;
     virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
-    virtual void move(point_t destination) = 0;
+    virtual void move(const point_t& destination) = 0;
     virtual void move(double moveX, double moveY) = 0;
     virtual void scale(double coefficient) = 0;
   };
