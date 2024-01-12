d552d27878915ff65c9714c741031579d5e017aa
shavel0k <anna.reznikova@icloud.com>
2024-01-12 04:49:02 +0300

    try to fix acceptance

--- reznikova.anna/T1/regular.cpp
+++ reznikova.anna/T1/regular.cpp
@@ -9,26 +9,11 @@ reznikova::Regular::Regular(point_t center, point_t point1, point_t point2):
 {
   double side1 = pow((pow((center_.x-point1_.x), 2) + pow((center_.y-point1_.y), 2)), 0.5);
   double side2 = pow((pow((center_.x-point2_.x), 2) + pow((center_.y-point2_.y), 2)), 0.5);
-  double vec1_x, vec1_y;
-  vec1_x = vec1_y = 0;
-  if (side1 < side2)
-  {
-    r = side1;
-    R = side2;
-    vec1_x = point1_.x - center_.x;
-    vec1_y = point1_.y - center_.y;
-  }
-  else
-  {
-    r = side2;
-    R = side1;
-    vec1_x = point2_.x - center_.x;
-    vec1_y = point2_.y - center_.y;
-  }
+  r = (side1 < side2 ? side1 : side2);
+  R = (side1 > side2 ? side1 : side2);
   num_sides = M_PI / (acos(r/R));
-  double vec2_x = point1_.x - point2_.x;
-  double vec2_y = point1_.y - point2_.y;
-  if ((vec1_x * vec2_x + vec1_y * vec2_y) != 0)
+  double epsilon = 0.0000001;
+  if (abs(num_sides - floor(num_sides + 0.5)) > epsilon)
   {
     throw std::runtime_error("wrong parameters\n");
   }
@@ -42,25 +27,42 @@ double reznikova::Regular::getArea() const
 
 reznikova::rectangle_t reznikova::Regular::getFrameRect() const
 {
-  double width = 2 * R;
-  double height = 2 * R;
-  rectangle_t rectangle(width, height, center_);
+  double height = 0;
+  if (abs(point1_.y - center_.y) > abs(point2_.y - center_.y))
+  {
+    height = 2 * abs(point1_.y - center_.y);
+  }
+  else
+  {
+    height = 2 * abs(point2_.y - center_.y);
+  }
+  double width = 0;
+  if (abs(point1_.x - center_.x) > abs(point2_.x - center_.x))
+  {
+    width = 2 * abs(point1_.x - center_.x);
+  }
+  else
+  {
+    width = 2 * abs(point2_.x - center_.x);
+  }
+  double res_len = (width > height ? width : height);
+  rectangle_t rectangle(res_len, res_len, center_);
   return rectangle;
 }
 
-void reznikova::Regular::move(double dx, double dy)
+void reznikova::Regular::move(const double dx, const double dy)
 {
   center_.x += dx;
   center_.y += dy;
 }
 
-void reznikova::Regular::move(point_t new_center)
+void reznikova::Regular::move(const point_t new_center)
 {
   center_.x = new_center.x;
   center_.y = new_center.y;
 }
 
-void reznikova::Regular::scale(double n)
+void reznikova::Regular::scale(const double n)
 {
   point1_.x *= n;
   point1_.y *= n;
@@ -68,14 +70,19 @@ void reznikova::Regular::scale(double n)
   point2_.y *= n;
 }
 
-void reznikova::Regular::newscale(point_t center, double n)
+void reznikova::Regular::newscale(const point_t center, const double n)
 {
   double p1_dx = point1_.x - center.x;
   double p1_dy = point1_.y - center.y;
   double p2_dx = point2_.x - center.x;
   double p2_dy = point2_.y - center.y;
+  double c_dx = center_.x - center.x;
+  double c_dy = center_.y - center.y;
   point1_.x = center.x + p1_dx * n;
   point1_.y = center.y + p1_dy * n;
   point2_.x = center.x + p2_dx * n;
   point2_.y = center.y + p2_dy * n;
+  center_.x = center.x + c_dx * n;
+  center_.y = center.y + c_dy * n;
+  r *= n;
 }
