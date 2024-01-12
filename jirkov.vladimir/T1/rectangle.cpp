063a502397e9e498999531d50c9e57b2f01cbb51
jirkov17 <JIRKOFF17@GMAIL.COM>
2024-01-12 22:27:18 +0300

    new retangle

--- jirkov.vladimir/T1/rectangle.cpp
+++ jirkov.vladimir/T1/rectangle.cpp
@@ -7,27 +7,27 @@ Rectangle::Rectangle(point_t bottomLeft, point_t topRight) :
 
 double Rectangle::getArea() const
 {
-  rectangle_t frame = getFrameRect();
-  return frame.width * frame.height;
+  double width = topRight_.x - bottomLeft_.x;
+  double height = bottomLeft_.y - topRight_.y;
+  double recSquare = width * height;
+  return recSquare;
 }
 rectangle_t Rectangle::getFrameRect() const
 {
   double width = topRight_.x - bottomLeft_.x;
   double height = topRight_.y - bottomLeft_.y;
-  double posX = (topRight_.x + bottomLeft_.x) * 0.5;
-  double posY = (topRight_.y + bottomLeft_.y) * 0.5;
-  point_t pos = { posX, posY };
+  point_t pos = { (topRight_.x + bottomLeft_.x) / 2, (topRight_.y + bottomLeft_.y) / 2 };
   return rectangle_t{ width, height, pos };
 }
-
 void Rectangle::move(const point_t & destination)
 {
-  rectangle_t frame = getFrameRect();
-  double moveX = destination.x - frame.pos.x;
-  double moveY = destination.y - frame.pos.y;
-  move(moveX, moveY);
+  double dXbottom = bottomLeft_.x + (destination.x - rectangle.pos.x);
+  double dYbottom = bottomLeft_.y + (destination.y - rectangle.pos.y);
+  double dXtop = topRight_.x + (destination.x - rectangle.pos.x);
+  double dYtop = topRight_.y + (destination.y - rectangle.pos.y);
+  bottomLeft_ = { dXbottom, dYbottom };
+  topRight_ = { dXtop, dYtop };
 }
-
 void Rectangle::move(double moveX, double moveY)
 {
   bottomLeft_.x += moveX;
