c9cd8da471d258ae9db6a67b38ce813ab5831c27
AlexNik <sahan@DESKTOP-R8OTJOJ>
2024-01-14 01:09:42 +0300

    test add ~shape

--- nikiforov.alexander/T1/shape.hpp
+++ nikiforov.alexander/T1/shape.hpp
@@ -11,6 +11,7 @@ namespace nikiforov
     virtual void move(double x, double y) = 0;
     virtual void move(point_t center) = 0;
     virtual void scale(double ratio) = 0;
+    virtual ~Shape() = default;
   };
 }
 
