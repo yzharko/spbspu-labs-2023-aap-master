0c49ad0e80a9df6b3d33c9fb0aaa0b13ce7b7d1f
Ignitron1 <iamremotr@gmail.com>
2024-01-13 12:15:43 -0500

    Added regular shape argument checking

--- smolyakov.alexander/T1/regular.cpp
+++ smolyakov.alexander/T1/regular.cpp
@@ -1,6 +1,4 @@
 #include "regular.hpp"
-#include <iostream>
-#include <limits>
 #include <stdexcept>
 #include <cmath>
 #include "base-types.hpp"
@@ -12,7 +10,7 @@ smolyakov::Regular::Regular(point_t center, point_t secondPoint, point_t thirdPo
 
 double smolyakov::Regular::getArea() const
 {
-  double sectorArea = outerRadius_ * outerRadius_ * sin(360.0 / sides_);
+  double sectorArea = 0.5 * outerRadius_ * outerRadius_ * sin(2.0 / sides_ * M_PI);
   return sectorArea * sides_;
 }
 
@@ -41,15 +39,24 @@ void smolyakov::Regular::scale(double factor)
 void smolyakov::Regular::calculateParameters(point_t center, point_t secondPoint, point_t thirdPoint)
 {
   const double epsilon = 1e-5;
+
   double firstSide = hypot(center.x - secondPoint.x, center.y - secondPoint.y);
   double secondSide = hypot(center.x - thirdPoint.x, center.y - thirdPoint.y);
+  double thirdSide = hypot(thirdPoint.x - secondPoint.x, thirdPoint.y - secondPoint.y);
+
+  bool isRightTriangle = (std::abs(firstSide * firstSide - secondSide * secondSide - thirdSide * thirdSide) < epsilon)
+    || (std::abs(secondSide * secondSide - thirdSide * thirdSide - firstSide * firstSide) < epsilon)
+    || (std::abs(thirdSide * thirdSide - firstSide * firstSide - secondSide * secondSide) < epsilon);
+  if (!isRightTriangle)
+  {
+    throw std::invalid_argument("The given triangle is not a right triangle.");
+  }
+
   innerRadius_ = firstSide < secondSide ? firstSide : secondSide;
   outerRadius_ = firstSide < secondSide ? secondSide : firstSide;
   double sides = M_PI / acos(innerRadius_ / outerRadius_);
   if (sides - round(sides) > epsilon)
   {
-    std::cout << sides << '\n';
-    std::cout << (long)sides << '\n';
     throw std::invalid_argument("Invalid regular shape control points.");
   }
   sides_ = round(sides);
