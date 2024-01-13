a8d733419d3fe31af003c365932cd1604e832199
Ignitron1 <iamremotr@gmail.com>
2024-01-13 15:55:31 -0500

    temporary main.cpp test file

--- smolyakov.alexander/T1/main.cpp
+++ smolyakov.alexander/T1/main.cpp
@@ -10,6 +10,15 @@ int main()
 {
   using namespace smolyakov;
 
-  Regular* regular = new Regular(point_t{0, 0}, point_t{0, 1}, point_t{1, 1});
-  std::cout << regular->getArea() << '\n';
+  Regular regular = Regular(point_t{0, 0}, point_t{0, 1}, point_t{1, 1});
+
+  Circle circle = Circle(point_t{100, 100}, 1.0);
+  Rectangle rectangle = Rectangle({5, 0}, {6, 1000});
+
+  CompositeShape compositeShape = CompositeShape(3);
+  compositeShape.addShape(regular);
+  compositeShape.addShape(circle);
+  compositeShape.addShape(rectangle);
+  std::cout << compositeShape.getFrameRect().width << '\n' << compositeShape.getFrameRect().height << '\n';
+  std::cout << compositeShape.getArea() << '\n';
 }
