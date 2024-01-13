0dd3d3ad793efb74baf10ea5e1ee721d070ab5bf
jirkov17 <JIRKOFF17@GMAIL.COM>
2024-01-13 13:00:38 +0300

    fix concave

--- jirkov.vladimir/T1/concave.cpp
+++ jirkov.vladimir/T1/concave.cpp
@@ -8,32 +8,20 @@ Concave::Concave(point_t firstPoint, point_t secondPoint, point_t thirdPoint, po
   thirdPoint_(thirdPoint),
   fourthPoint_(fourthPoint)
 {}
-
 double Concave::getArea() const
 {
-  double x1 = firstPoint_.x;
-  double y1 = firstPoint_.y;
-  double x2 = secondPoint_.x;
-  double y2 = secondPoint_.y;
-  double x3 = thirdPoint_.x;
-  double y3 = thirdPoint_.y;
-  double x4 = fourthPoint_.x;
-  double y4 = fourthPoint_.y;
-
-  double firstArea = (x1 * (y2 - y3)) + (x2 * (y3 - y1)) + (x3 * (y1 - y2)) / 2.0;
-  double secondArea = (x4 * (y2 - y3)) + (x2 * (y3 - y4)) + (x3 * (y4 - y2)) / 2;
-  return std::abs(firstArea - secondArea);
+  double bigTriangle = firstPoint_.x * (secondPoint_.y - thirdPoint_.y) + secondPoint_.x * (thirdPoint_.y - firstPoint_.y);
+  bigTriangle = std::abs((bigTriangle + thirdPoint_.x * (firstPoint_.y - secondPoint_.y)) / 2.0);
+  double smallTriangle = fourthPoint_.x * (secondPoint_.y - thirdPoint_.y) + secondPoint_.x * (thirdPoint_.y - fourthPoint_.y);
+  smallTriangle = std::abs((smallTriangle + thirdPoint_.x * (fourthPoint_.y - secondPoint_.y)) / 2.0);
+  return bigTriangle - smallTriangle;
 }
 rectangle_t Concave::getFrameRect() const
 {
-  double maxX = firstPoint_.x >= secondPoint_.x ? firstPoint_.x : secondPoint_.x;
-  maxX = maxX >= thirdPoint_.x ? maxX : thirdPoint_.x;
-  double minX = firstPoint_.x <= secondPoint_.x ? firstPoint_.x : secondPoint_.x;
-  minX = minX <= thirdPoint_.x ? minX : thirdPoint_.x;
-  double maxY = firstPoint_.y >= secondPoint_.y ? firstPoint_.y : secondPoint_.y;
-  maxY = maxY >= thirdPoint_.y ? maxY : thirdPoint_.y;
-  double minY = firstPoint_.y <= secondPoint_.y ? firstPoint_.y : secondPoint_.y;
-  minY = minY <= thirdPoint_.y ? minY : thirdPoint_.y;
+  double maxX = std::max({ firstPoint_.x, secondPoint_.x, thirdPoint_.x });
+  double minX = std::min({ firstPoint_.x, secondPoint_.x, thirdPoint_.x });
+  double maxY = std::max({ firstPoint_.y, secondPoint_.y, thirdPoint_.y });
+  double minY = std::min({ firstPoint_.y, secondPoint_.y, thirdPoint_.y });
   double width = maxX - minX;
   double height = maxY - minY;
   double posX = (maxX + minX) * 0.5;
@@ -41,7 +29,6 @@ rectangle_t Concave::getFrameRect() const
   point_t pos = { posX, posY };
   return rectangle_t{ width, height, pos };
 }
-
 void Concave::move(const point_t & destination)
 {
   double moveX = destination.x - fourthPoint_.x;
@@ -63,13 +50,20 @@ void Concave::move(double moveX, double moveY)
 
 void Concave::scale(double k)
 {
-  point_t pos;
-  pos.x = fourthPoint_.x;
-  pos.y = fourthPoint_.y;
-  firstPoint_.x += (firstPoint_.x - pos.x) * (k - 1);
-  firstPoint_.y += (firstPoint_.y - pos.y) * (k - 1);
-  secondPoint_.x += (secondPoint_.x - pos.x) * (k - 1);
-  secondPoint_.y += (secondPoint_.y - pos.y) * (k - 1);
-  thirdPoint_.x += (thirdPoint_.x - pos.x) * (k - 1);
-  thirdPoint_.y += (thirdPoint_.y - pos.y) * (k - 1);
+  point_t pos = fourthPoint_;
+
+  double dx = (firstPoint_.x - pos.x) * (k - 1);
+  double dy = (firstPoint_.y - pos.y) * (k - 1);
+  firstPoint_.x += dx;
+  firstPoint_.y += dy;
+
+  dx = (secondPoint_.x - pos.x) * (k - 1);
+  dy = (secondPoint_.y - pos.y) * (k - 1);
+  secondPoint_.x += dx;
+  secondPoint_.y += dy;
+
+  dx = (thirdPoint_.x - pos.x) * (k - 1);
+  dy = (thirdPoint_.y - pos.y) * (k - 1);
+  thirdPoint_.x += dx;
+  thirdPoint_.y += dy;
 }
