f45623bcfb3e298f0a21ceae72bad64b05a3165f
sadofeeeva <klover200015@gmail.com>
2024-01-14 02:35:00 +0700

    add

--- sadofeva.nastya/T1/rectangle.cpp
+++ sadofeva.nastya/T1/rectangle.cpp
@@ -1,3 +1,4 @@
+#include <stdexcept>
 #include "base-tapes.h"
 #include "rectangle.h"
 
@@ -24,4 +25,13 @@ void sadofeva::Rectangle::move(double _x, double _y)
 {
   rectangle_.pos.x += _x;
   rectangle_.pos.y += _y;
+}
+
+sadofeva::Rectangle::Recnagle(Point_t point1, Point_t point2):
+  rectangle_({{(point1.x + point2.y) / 2.0 , (point1.y + point2.y) / 2.0} , point2.x - point1.x, point2.y - point1.y })
+{
+  if (rectangle_.width <= 0.0 || rectangle_.height <= 0.0)
+  {
+    throw std::logic_error("not size");
+  }
 }
