3d67169d4f31b146f584df82c1462794cb864217
jirkov17 <JIRKOFF17@GMAIL.COM>
2024-01-13 04:55:50 +0300

    fixed new concave try 7

--- jirkov.vladimir/T1/concave.cpp
+++ jirkov.vladimir/T1/concave.cpp
@@ -1,7 +1,6 @@
 #include "concave.hpp"
 #include <cmath>
 #include <stdexcept>
-#include <algorithm>
 using namespace jirkov;
 Concave::Concave(point_t firstPoint, point_t secondPoint, point_t thirdPoint, point_t fourthPoint) :
   firstPoint_(firstPoint),
@@ -10,34 +9,64 @@ Concave::Concave(point_t firstPoint, point_t secondPoint, point_t thirdPoint, po
   fourthPoint_(fourthPoint)
 {}
 
-double Concave::getArea() const {
-  double x1 = firstPoint_.x;
-  double y1 = firstPoint_.y;
-  double x2 = secondPoint_.x;
-  double y2 = secondPoint_.y;
-  double x3 = thirdPoint_.x;
-  double y3 = thirdPoint_.y;
-  double x4 = fourthPoint_.x;
-  double y4 = fourthPoint_.y;
-
-  double firstArea = (x1 * (y2 - y3)) + (x2 * (y3 - y1)) + (x3 * (y1 - y2));
-  firstArea = std::abs(firstArea / 2.0);
-
-  double secondArea = (x4 * (y2 - y3)) + (x2 * (y3 - y4)) + (x3 * (y4 - y2));
-  secondArea = std::abs(secondArea / 2.0);
-
-  return firstArea - secondArea;
+double Concave::getArea() const
+{
+  double firstSqrt = sqrt(pow((firstPoint_.x - secondPoint_.x), 2) + pow((firstPoint_.y - secondPoint_.y), 2));
+  double secondSqrt = sqrt(pow((secondPoint_.x - thirdPoint_.x), 2) + pow((secondPoint_.y - thirdPoint_.y), 2));
+  double thirdSqrt = sqrt(pow((thirdPoint_.x - firstPoint_.x), 2) + pow((thirdPoint_.y - firstPoint_.y), 2));
+  double mediumSqrt = (firstSqrt + secondSqrt + thirdSqrt) * 0.5;
+  double firstSquare = sqrt(mediumSqrt * (mediumSqrt - firstSqrt) * (mediumSqrt - secondSqrt) * (mediumSqrt - thirdSqrt));
+  double maxSide = firstSqrt >= secondSqrt ? firstSqrt : secondSqrt;
+  maxSide = maxSide >= thirdSqrt ? maxSide : thirdSqrt;
+  if (maxSide >= firstSqrt + secondSqrt + thirdSqrt - maxSide)
+  {
+    throw std::logic_error("Triangle error\n");
+  }
+  double fourthSqrt = sqrt(pow((fourthPoint_.x - secondPoint_.x), 2) + pow((fourthPoint_.y - secondPoint_.y), 2));
+  double fifthSqrt = sqrt(pow((secondPoint_.x - thirdPoint_.x), 2) + pow((secondPoint_.y - thirdPoint_.y), 2));
+  double sixthSqrt = sqrt(pow((thirdPoint_.x - fourthPoint_.x), 2) + pow((thirdPoint_.y - fourthPoint_.y), 2));
+  double seventhSqrt = (fourthSqrt + fifthSqrt + sixthSqrt) * 0.5;
+  double secondSquare = sqrt(seventhSqrt * (seventhSqrt - fourthSqrt) * (seventhSqrt - fifthSqrt) * (seventhSqrt - sixthSqrt));
+  maxSide = fourthSqrt >= fifthSqrt ? fourthSqrt : fifthSqrt;
+  maxSide = maxSide >= sixthSqrt ? maxSide : sixthSqrt;
+  if (maxSide >= fourthSqrt + fifthSqrt + sixthSqrt - maxSide)
+  {
+    throw std::logic_error("Triangle error\n");
+  }
+  double eighthSqrt = sqrt(pow((firstPoint_.x - secondPoint_.x), 2) + pow((firstPoint_.y - secondPoint_.y), 2));
+  double ninthSqrt = sqrt(pow((secondPoint_.x - fourthPoint_.x), 2) + pow((secondPoint_.y - fourthPoint_.y), 2));
+  double tenthSqrt = sqrt(pow((fourthPoint_.x - firstPoint_.x), 2) + pow((fourthPoint_.y - firstPoint_.y), 2));
+  maxSide = eighthSqrt >= ninthSqrt ? eighthSqrt : ninthSqrt;
+  maxSide = maxSide >= tenthSqrt ? maxSide : tenthSqrt;
+  if (maxSide >= eighthSqrt + ninthSqrt + tenthSqrt - maxSide)
+  {
+    throw std::logic_error("Triangle error\n");
+  }
+  eighthSqrt = sqrt(pow((firstPoint_.x - thirdPoint_.x), 2) + pow((firstPoint_.y - thirdPoint_.y), 2));
+  ninthSqrt = sqrt(pow((thirdPoint_.x - fourthPoint_.x), 2) + pow((thirdPoint_.y - fourthPoint_.y), 2));
+  tenthSqrt = sqrt(pow((fourthPoint_.x - firstPoint_.x), 2) + pow((fourthPoint_.y - firstPoint_.y), 2));
+  maxSide = eighthSqrt >= ninthSqrt ? eighthSqrt : ninthSqrt;
+  maxSide = maxSide >= tenthSqrt ? maxSide : tenthSqrt;
+  if (maxSide >= eighthSqrt + ninthSqrt + tenthSqrt - maxSide)
+  {
+    throw std::logic_error("Triangle error\n");
+  }
+  return firstSquare - secondSquare;
 }
 rectangle_t Concave::getFrameRect() const
 {
-  double maxX = std::max({ firstPoint_.x, secondPoint_.x, thirdPoint_.x });
-  double minX = std::min({ firstPoint_.x, secondPoint_.x, thirdPoint_.x });
-  double maxY = std::max({ firstPoint_.y, secondPoint_.y, thirdPoint_.y });
-  double minY = std::min({ firstPoint_.y, secondPoint_.y, thirdPoint_.y });
+  double maxX = firstPoint_.x >= secondPoint_.x ? firstPoint_.x : secondPoint_.x;
+  maxX = maxX >= thirdPoint_.x ? maxX : thirdPoint_.x;
+  double minX = firstPoint_.x <= secondPoint_.x ? firstPoint_.x : secondPoint_.x;
+  minX = minX <= thirdPoint_.x ? minX : thirdPoint_.x;
+  double maxY = firstPoint_.y >= secondPoint_.y ? firstPoint_.y : secondPoint_.y;
+  maxY = maxY >= thirdPoint_.y ? maxY : thirdPoint_.y;
+  double minY = firstPoint_.y <= secondPoint_.y ? firstPoint_.y : secondPoint_.y;
+  minY = minY <= thirdPoint_.y ? minY : thirdPoint_.y;
   double width = maxX - minX;
   double height = maxY - minY;
-  double posX = (minX + width) * 0.5;
-  double posY = (minY + height) * 0.5;
+  double posX = (maxX + minX) * 0.5;
+  double posY = (maxY + minY) * 0.5;
   point_t pos = { posX, posY };
   return rectangle_t{ width, height, pos };
 }
@@ -47,15 +76,6 @@ void Concave::move(const point_t & destination)
   double moveX = destination.x - fourthPoint_.x;
   double moveY = destination.y - fourthPoint_.y;
   move(moveX, moveY);
-  firstPoint_.x += moveX;
-  firstPoint_.y += moveY;
-  secondPoint_.x += moveX;
-  secondPoint_.y += moveY;
-  thirdPoint_.x += moveX;
-  thirdPoint_.y += moveY;
-  fourthPoint_.x += moveX;
-  fourthPoint_.y += moveY;
-
 }
 
 void Concave::move(double moveX, double moveY)
@@ -72,10 +92,13 @@ void Concave::move(double moveX, double moveY)
 
 void Concave::scale(double k)
 {
-  firstPoint_.x = fourthPoint_.x + (firstPoint_.x - fourthPoint_.x) * k;
-  firstPoint_.y = fourthPoint_.y + (firstPoint_.y - fourthPoint_.y) * k;
-  secondPoint_.x = fourthPoint_.x + (secondPoint_.x - fourthPoint_.x) * k;
-  secondPoint_.y = fourthPoint_.y + (secondPoint_.y - fourthPoint_.y) * k;
-  thirdPoint_.x = fourthPoint_.x + (thirdPoint_.x - fourthPoint_.x) * k;
-  thirdPoint_.y = fourthPoint_.y + (thirdPoint_.y - fourthPoint_.y) * k;
+  point_t pos;
+  pos.x = fourthPoint_.x;
+  pos.y = fourthPoint_.y;
+  firstPoint_.x += (firstPoint_.x - pos.x) * (k - 1);
+  firstPoint_.y += (firstPoint_.y - pos.y) * (k - 1);
+  secondPoint_.x += (secondPoint_.x - pos.x) * (k - 1);
+  secondPoint_.y += (secondPoint_.y - pos.y) * (k - 1);
+  thirdPoint_.x += (thirdPoint_.x - pos.x) * (k - 1);
+  thirdPoint_.y += (thirdPoint_.y - pos.y) * (k - 1);
 }
