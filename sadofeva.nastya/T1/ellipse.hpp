4990a7f828e15ed1874ae03979e0011f3911c1cd
sadofeeeva <klover200015@gmail.com>
2024-01-14 02:09:59 +0700

    add

---
+++ sadofeva.nastya/T1/ellipse.hpp
@@ -0,0 +1,21 @@
+#ifndef ELLIPSE_HPP
+#define ELLIPSE_HPP
+#include "base-tapes.hpp"
+#include "shape.hpp"
+
+namespace sadofeva
+{
+  class Ellipse : public Shape
+  {
+    Ellipse(Point_t point, double r_1, double r_2);
+    double getArea() const override;
+    Rectangle_t getFrameRect() const override;
+    void move(double _x, double _y) override;
+    void move( Point_t point) override;
+  private:
+    double r1,r2;
+    Rectangle_t rectangle;
+  }
+}
+
+#endif
--- sadofeva.nastya/T1/main.cpp
+++ sadofeva.nastya/T1/main.cpp
@@ -1,1 +1,25 @@
-///
+#include <iostream>
+#include "base-tapes.hpp"
+#include "shape.hpp"
+#include "rectangle.hpp"
+#include "helpfunct.hpp"
+
+
+int main()
+{
+  while (std::cin)
+  {
+    std::string figuraname = "";
+    std::cin >> figuraname;
+    if (!std::cin)
+    {
+      break;
+    }
+    if (figuraname == "ELLIPSE")
+    {
+    }
+    if (figuraname == "COMPLEXQUAD")
+    {
+    }
+  }
+}
