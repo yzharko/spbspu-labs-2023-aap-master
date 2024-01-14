c49e2591ad00ad4c85dc09ba928e566f42e6bff5
Ignitron1 <iamremotr@gmail.com>
2024-01-13 20:23:38 -0500

    An attempt to fix a valgrind error

--- smolyakov.alexander/T1/main.cpp
+++ smolyakov.alexander/T1/main.cpp
@@ -39,7 +39,6 @@ int main()
         for (size_t i = 0; i < shapeCount; i++)
         {
           newShapes[i] = shapes[i];
-          delete shapes[i];
         }
         delete[] shapes;
         shapes = newShapes;
