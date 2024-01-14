4a63bcd0f78a4266a701c0efab116c004a5750a3
Likhachev Aleksey <lavran03@gmail.com>
2024-01-14 04:39:37 +0300

    (stage) Изменение выделения памяти
    Ранее массив под параметры был статическим. Теперь он динамический, и удаляется после ввода всех значений

--- likhachev.aleksey/T1/main.cpp
+++ likhachev.aleksey/T1/main.cpp
@@ -12,15 +12,16 @@ int main()
 
   size_t shapeCount = 0;
   likhachev::Shape *shapes[1000];
-  double shapeParams[10] {0};
 
   std::string shapeName = "";
   while (std::cin >> shapeName) {
+     double *shapeParams = nullptr;
     if (shapeName == "PARALLELOGRAM" || shapeName == "RING" || shapeName == "RECTANGLE") {
       // Lavran TODO: Присутствуют повтоерия. Попробовать заменить.
       int paramsCount = 0;
       if (shapeName == "PARALLELOGRAM") {
         paramsCount = 6;
+        shapeParams = new double[paramsCount];
         for (int i = 0; i < paramsCount; i++) {
           std::cin >> shapeParams[i];
         }
@@ -31,6 +32,7 @@ int main()
 
       if (shapeName == "RING") {
         paramsCount = 4;
+        shapeParams = new double[paramsCount];
         for (int i = 0; i < paramsCount; i++) {
           std::cin >> shapeParams[i];
         }
@@ -40,12 +42,14 @@ int main()
 
       if (shapeName == "RECTANGLE") {
         paramsCount = 4;
+        shapeParams = new double[paramsCount];
         for (int i = 0; i < paramsCount; i++) {
           std::cin >> shapeParams[i];
         }
         shapes[shapeCount] = new likhachev::Rectangle({shapeParams[0], shapeParams[1]},
                                                       {shapeParams[2], shapeParams[3]});\
       }
+      delete[] shapeParams;
       shapeCount++;
     }
   }
