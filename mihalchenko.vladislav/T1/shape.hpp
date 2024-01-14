05c8400b409d48cb0285c4474f69df139e38584d
Vlad0sEnIgma345 <vlad0senigma345@gmail.com>
2024-01-14 16:20:21 +0300

    Try to fix dinamic

--- mihalchenko.vladislav/T1/shape.hpp
+++ mihalchenko.vladislav/T1/shape.hpp
@@ -7,6 +7,7 @@ namespace mihalchenko
   class Shape
   {
   public:
+    virtual ~Shape() = default;
     virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
     virtual void move(const point_t &point) = 0;
