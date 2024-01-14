4c60b1b88c61b2cdf5f244dda35b67b0453e096e
AlexNik <sahan@DESKTOP-R8OTJOJ>
2024-01-14 03:48:54 +0300

    fixed shape

--- nikiforov.alexander/T1/shape.hpp
+++ nikiforov.alexander/T1/shape.hpp
@@ -6,6 +6,7 @@ namespace nikiforov
 {
   class Shape
   {
+  public:
     virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
     virtual void move(double x, double y) = 0;
