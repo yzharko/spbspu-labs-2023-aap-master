24010d99ef8e2a82b966b10665924980158a5f17
Алексей Серый <seryj.av@edu.spbstu.ru>
2024-01-14 18:42:29 +0300

    порядок восстановлен

--- seryj.alexey/T1/shape.hpp
+++ seryj.alexey/T1/shape.hpp
@@ -5,12 +5,12 @@ namespace seryj {
   class Shape
   {
   public:
+    virtual ~Shape() = default;
     virtual double getArea()const = 0;
     virtual rectangle_t getFrameRect()const = 0;
     virtual void move(const point_t&) = 0;
     virtual void move(const double& x, const double& y) = 0;
     virtual void scale(double k) = 0;
-    virtual ~Shape() = default;
   };
 }
 #endif
