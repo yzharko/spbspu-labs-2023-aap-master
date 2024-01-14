a639b4944b05a344fce0e625f4ea719c88d074fd
Likhachev Aleksey <lavran03@gmail.com>
2024-01-14 20:26:40 +0300

    (fix) Изменение операторов point_t
    Ранее операторы "+" и "-" меняли значения параметров

--- likhachev.aleksey/T1/base-types.cpp
+++ likhachev.aleksey/T1/base-types.cpp
@@ -8,16 +8,12 @@ likhachev::point_t::point_t(double newX, double newY):
 
 likhachev::point_t likhachev::point_t::operator+(const point_t& rhs)
 {
-  x += rhs.x;
-  y += rhs.y;
-  return *this;
+  return likhachev::point_t(x + rhs.x, y + rhs.y);
 }
 
 likhachev::point_t likhachev::point_t::operator-(const point_t& rhs)
 {
-  x -= rhs.x;
-  y -= rhs.y;
-  return *this;
+  return likhachev::point_t(x - rhs.x, y - rhs.y);
 }
 
 bool likhachev::point_t::operator==(const point_t& rhs)
@@ -31,13 +27,15 @@ bool likhachev::point_t::operator==(const point_t& rhs)
 
 likhachev::point_t& likhachev::point_t::operator+=(const point_t& rhs)
 {
-  *this = *this + rhs;
+  x += rhs.x;
+  y += rhs.y;
   return *this;
 }
 
 likhachev::point_t& likhachev::point_t::operator-=(const point_t& rhs)
 {
-  *this = *this - rhs;
+  x -= rhs.x;
+  y -= rhs.y;
   return *this;
 }
 
@@ -46,3 +44,44 @@ likhachev::rectangle_t::rectangle_t(double newWidth, double newHeight, likhachev
   height(newHeight),
   pos(newPos)
 {}
+
+double likhachev::rectangle_t::getArea() const
+{
+  return width * height;
+}
+
+double likhachev::rectangle_t::getWidth() const
+{
+  return width;
+}
+
+double likhachev::rectangle_t::getHeight() const
+{
+  return height;
+}
+
+likhachev::point_t likhachev::rectangle_t::getPos() const
+{
+  return pos;
+}
+
+void likhachev::rectangle_t::setWidth(double newWidth)
+{
+  if (width <= 0) {
+    throw std::logic_error("Error: the width of the figure cannot be non-positive\n");
+  }
+  width = newWidth;
+}
+
+void likhachev::rectangle_t::setHeight(double newHeight)
+{
+  if (height <= 0) {
+    throw std::logic_error("Error: the height of the figure cannot be non-positive\n");
+  }
+  height = newHeight;
+}
+
+void likhachev::rectangle_t::setPos(likhachev::point_t newPos)
+{
+  pos = newPos;
+}
