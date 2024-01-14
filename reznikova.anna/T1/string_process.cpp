421a2d323b1dfafb091c1373f87938a882d33cd2
shavel0k <anna.reznikova@icloud.com>
2024-01-14 23:25:26 +0300

    I dont believe it will help, but...

--- reznikova.anna/T1/string_process.cpp
+++ reznikova.anna/T1/string_process.cpp
@@ -16,8 +16,9 @@ reznikova::point_t reznikova::getPointFromString(std::string& line)
   size_t fir_ptr = 0;
   size_t sec_ptr = 0;
   double x = std::stod(line, &fir_ptr);
-  double y = std::stod(line.substr(fir_ptr), &sec_ptr);
-  line = line.substr(fir_ptr+sec_ptr);
+  line = line.substr(fir_ptr);
+  double y = std::stod(line, &sec_ptr);
+  line = line.substr(sec_ptr);
   point_t point{x, y};
   return point;
 }
