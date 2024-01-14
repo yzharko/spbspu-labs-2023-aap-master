5bfa6e33e51e2d920787fcc457e22b07b51541cb
Likhachev Aleksey <lavran03@gmail.com>
2024-01-14 06:36:56 +0300

    (fix) Работа с CG

--- likhachev.aleksey/T1/utils/shape-functions.cpp
+++ likhachev.aleksey/T1/utils/shape-functions.cpp
@@ -8,7 +8,7 @@ void likhachev::coutShapesData(Shape **shapes, size_t shapeCount)
     areaSum += shapes[i]->getArea();
   }
 
-  std::cout << "	" << areaSum;
+  std::cout << areaSum;
   for (size_t i = 0; i < shapeCount; i++) {
     likhachev::Rectangle_t frameRect = shapes[i]->getFrameRect();
     likhachev::Point_t lowerLeft = frameRect.getPos() - likhachev::Point_t(frameRect.getWidth() / 2, frameRect.getHeight() / 2);
