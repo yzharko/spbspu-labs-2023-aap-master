9877710567a2c740829aa407b8cf314a07c8822c
Алексей Серый <seryj.av@edu.spbstu.ru>
2024-01-14 18:43:34 +0300

    плохие имена устранены

--- seryj.alexey/T1/regular.cpp
+++ seryj.alexey/T1/regular.cpp
@@ -7,12 +7,12 @@
 using namespace seryj;
 Regular::Regular(point_t a, point_t b, point_t c)
 {
-  p_b_ = b;
-  p_c_ = c;
+  point_b_ = b;
+  point_c_ = c;
   pos_ = a;
-  double side_ab = sqrt(pow(pos_.x - p_b_.x, 2) + pow(pos_.y - p_b_.y, 2));
-  double side_ac = sqrt(pow(pos_.x - p_c_.x, 2) + pow(pos_.y - p_c_.y, 2));
-  double side_bc = sqrt(pow(p_b_.x - p_c_.x, 2) + pow(p_b_.y - p_c_.y, 2));
+  double side_ab = sqrt(pow(pos_.x - point_b_.x, 2) + pow(pos_.y - point_b_.y, 2));
+  double side_ac = sqrt(pow(pos_.x - point_c_.x, 2) + pow(pos_.y - point_c_.y, 2));
+  double side_bc = sqrt(pow(point_b_.x - point_c_.x, 2) + pow(point_b_.y - point_c_.y, 2));
   hypotenuse_ = fmax(side_ab, side_ac);
   n_ = M_PI / cos(fmin(side_ab, side_ac) / hypotenuse_);
   if (round(side_bc * 100) != round(hypotenuse_ * sin(M_PI / n_) * 100))
@@ -24,7 +24,7 @@ double Regular::getArea()const
 }
 rectangle_t Regular::getFrameRect()const
 {
-  double rec_side = fmax(fmax(abs(p_b_.x - pos_.x), abs(p_c_.x - pos_.x)), fmax(abs(p_b_.y - pos_.y), abs(p_c_.y - pos_.y)));
+  double rec_side = fmax(fmax(abs(point_b_.x - pos_.x), abs(point_c_.x - pos_.x)), fmax(abs(point_b_.y - pos_.y), abs(point_c_.y - pos_.y)));
   return rectangle_t{rec_side * 2, rec_side * 2, pos_ };
 }
 void Regular::move(const point_t& dest)
@@ -38,8 +38,8 @@ void Regular::move(const double& x,const double& y)
 void Regular::scale(double k)
 {
   hypotenuse_ *= k;
-  p_b_.x *= k;
-  p_b_.y *= k;
-  p_c_.x *= k;
-  p_c_.y *= k;
+  point_b_.x *= k;
+  point_b_.y *= k;
+  point_c_.x *= k;
+  point_c_.y *= k;
 }
--- seryj.alexey/T1/regular.hpp
+++ seryj.alexey/T1/regular.hpp
@@ -15,8 +15,8 @@ namespace seryj {
     int n_ = 0;
     double hypotenuse_ = 0;
     point_t pos_ = { 0,0 };
-    point_t p_b_ = { 0,0 };
-    point_t p_c_ = { 0,0 };
+    point_t point_b_ = { 0,0 };
+    point_t point_c_ = { 0,0 };
   };
 }
 #endif
