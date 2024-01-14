f2c97b9cbebfaa72a68696fd8629a944ecb22a9e
Vlad0sEnIgma345 <vlad0senigma345@gmail.com>
2024-01-14 15:44:56 +0300

    Trying to fix CG

--- mihalchenko.vladislav/T1/funcInOut.hpp
+++ mihalchenko.vladislav/T1/funcInOut.hpp
@@ -6,20 +6,20 @@
 
 namespace mihalchenko
 {
-    bool isPoligon(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4);
+  bool isPoligon(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4);
 
-    Shape **resizeArrayOfShapes(Shape **arrayGeomShapes, size_t sizeOfArrShapes);
+  Shape **resizeArrayOfShapes(Shape **arrayGeomShapes, size_t sizeOfArrShapes);
 
-    void delDinStorage(Shape **arrayGeomShapes, size_t shapeCounter);
+  void delDinStorage(Shape **arrayGeomShapes, size_t shapeCounter);
 
-    size_t recordingArrayOfShapes(Shape **arrayGeomShapes, size_t shapeCounter, size_t markerGeomShape);
+  size_t recordingArrayOfShapes(Shape **arrayGeomShapes, size_t shapeCounter, size_t markerGeomShape);
 
-    size_t processingAndOut(Shape **arrayGeomShapes, size_t shapeCounter);
+  size_t processingAndOut(Shape **arrayGeomShapes, size_t shapeCounter);
 
-    point_t *dinResize(point_t *points, size_t counterOfPoints, size_t sizeArrayOfPoints, const size_t addArrayOfPoints);
+  point_t *dinResize(point_t *points, size_t counterOfPoints, size_t sizeArrayOfPoints, const size_t addArrayOfPoints);
 
-    void sortingShell(double *arr[], size_t num);
+  void sortingShell(double *arr[], size_t num);
 
-    void savePoligon(size_t count, point_t *points);
+  void savePoligon(size_t count, point_t *points);
 }
 #endif
