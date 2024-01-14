95fe78ac36240547f8593959bbfd500defe28cc4
AlexNik <sahan@DESKTOP-R8OTJOJ>
2024-01-14 03:56:38 +0300

    add ~shape

--- nikiforov.alexander/T1/shape.hpp
+++ nikiforov.alexander/T1/shape.hpp
@@ -7,6 +7,7 @@ namespace nikiforov
   class Shape
   {
   public:
+    virtual ~Shape() {};
     virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
     virtual void move(double x, double y) = 0;
