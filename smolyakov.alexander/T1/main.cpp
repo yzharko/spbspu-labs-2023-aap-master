f031fa40c573f4db61836d1f9e102d0826f2dee7
Ignitron1 <iamremotr@gmail.com>
2024-01-13 20:36:38 -0500

    An attempt to fix a valgrind error

--- smolyakov.alexander/T1/main.cpp
+++ smolyakov.alexander/T1/main.cpp
@@ -77,7 +77,11 @@ int main()
   {
     std::cerr << "An invalid shape was entered.\n";
   }
-
+  
+  for (size_t i = 0; i < shapeCount; i++)
+  {
+    delete shapes[i];
+  }
   delete[] shapes;
   return 0;
 }
