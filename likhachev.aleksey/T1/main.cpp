552c896d6f69eb93a0056635d08d15f0694910db
Likhachev Aleksey <lavran03@gmail.com>
2024-01-14 03:08:30 +0300

    (stage) Ввод фигур

--- likhachev.aleksey/T1/main.cpp
+++ likhachev.aleksey/T1/main.cpp
@@ -6,6 +6,48 @@
 
 int main()
 {
-  std::cout << "Hi";
+  std::cout << "Hi"; // Lavran TODO: Удалить (Талисман :>)
+  
+
+  size_t shapeCount = 0;
+  likhachev::Shape *shapes[1000];
+
+  std::string shapeName = "";
+  double *shapeParams = nullptr;
+  while (std::cin >> shapeName) {
+    if(shapeName == "PARALLELOGRAM" || shapeName == "RING" || shapeName == "RECTANGLE") { // Lavran TODO: Присутствуют повтоерия. Попробовать заменить.
+      int paramsCount = 0;
+      if(shapeName == "PARALLELOGRAM") {
+        paramsCount = 6;
+        shapeParams = new double[paramsCount];
+        for(int i = 0; i < paramsCount; i++) {
+          std::cin >> shapeParams[i];
+        }
+        shapes[shapeCount] = new likhachev::Parallelogram({shapeParams[0], shapeParams[1]}, {shapeParams[2], shapeParams[3]}, {shapeParams[4], shapeParams[5]});
+      }
+
+      if(shapeName == "RING") {
+        paramsCount = 4;
+        shapeParams = new double[paramsCount];
+        for(int i = 0; i < paramsCount; i++) {
+          std::cin >> shapeParams[i];
+        }
+        shapes[shapeCount] = new likhachev::Ring({shapeParams[0], shapeParams[1]}, shapeParams[2], shapeParams[3]);
+      }
+
+      if(shapeName == "RECTANGLE") {
+        paramsCount = 4;
+        shapeParams = new double[paramsCount];
+        for(int i = 0; i < paramsCount; i++) {
+          std::cin >> shapeParams[i];
+        }
+        shapes[shapeCount] = new likhachev::Rectangle({shapeParams[0], shapeParams[1]}, {shapeParams[2], shapeParams[3]});
+      }
+
+      delete[] shapeParams;
+      shapeParams = nullptr;
+      shapeCount++;
+    }
+  }
   return 1;
 }
