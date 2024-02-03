32613cb091c9db2c0f1c9f8d14a99e1e9040f746
yva <yartsev-va@yandex.ru>
2024-02-03 15:45:02 +0300

    Reworked structure of the code since solutions for num == 1 and num == 2 were too similar

--- yartsev.viktor/P4/main.cpp
+++ yartsev.viktor/P4/main.cpp
@@ -28,28 +28,23 @@ int main(int argc, char ** argv)
   int rows = 0, cols = 0;
   std::ifstream input(argv[2]);
   input >> rows >> cols;
+  int a[10000] = {};
+  int * matrix = nullptr;
   if (num == 1) {
-    int matrix[10000] = {};
-    try {
-      yartsev::readArray(matrix, cols, rows, input);
-    } catch (const std::logic_error & e) {
-      std::cerr << e.what() << "\n";
-      return 2;
-    }
-    if (yartsev::isLowerTriangularMatrix(matrix, cols, rows)) {
-        std::cout << "Is a triangular matrix\n";
-    } else {
-        std::cout << "Not a triangular matrix\n";
-    }
-    return 0;
+    matrix = a;
   } else {
-    int * matrix = new int[rows * cols];
-    try {
-      yartsev::readArray(matrix, cols, rows, input);
-    } catch (const std::logic_error & e) {
-      std::cerr << e.what() << "\n";
-      return 2;
-    }
+    matrix = new int [rows * cols];
+  }
+  try {
+    yartsev::readArray(matrix, cols, rows, input);
+  } catch (const std::logic_error & e) {
+    std::cerr << e.what() << "\n";
+    return 2;
+  }
+  if (yartsev::isLowerTriangularMatrix(matrix, cols, rows)) {
+    std::cout << "Is triangular matrix\n";
+  } else {
+    std::cout << "Not a triangular matrix\n";
   }
 
   return 0;
