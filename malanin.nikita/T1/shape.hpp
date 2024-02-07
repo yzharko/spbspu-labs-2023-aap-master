1d94f61be6b4ce9998f96506874d155c202609b2
zeneco <nmalanin05@gmail.com>
2024-02-07 10:07:15 +0300

    try to fix git

--- malanin.nikita/T1/shape.hpp
+++ malanin.nikita/T1/shape.hpp
@@ -1,12 +1,15 @@
 #ifndef SHAPE_HPP
 #define SHAPE_HPP
-#include "base_types.hpp"
 
-namespace malanin
+#include "base-types.hpp"
+
+class Shape
 {
-  class Shape
-  {
-    public:
-      
-  }
-}
+public:
+    virtual double getArea() const = 0; // вычисление площади
+    virtual malanin::rectangle_t getFrameRect() const = 0; // получение ограничивающего прямоугольника для фигуры
+    virtual void move(const malanin::point_t & newPos) = 0; // перемещение  в конкретную точку
+    virtual void move(double dx, double dy) = 0; //  в виде смещений по осям абсцисс и ординат(координат)
+    virtual void scale(double scaleFactor) = 0; // изотропное масштабирование относительно центра с коэф.
+};
+#endif
