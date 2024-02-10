e6bc1666457e251f39adc03ed540037da73ec4f2
yva <yartsev-va@yandex.ru>
2024-02-10 23:56:44 +0300

    added files for 1st variant

---
+++ yartsev.viktor/P3/spaceremoval.cpp
@@ -0,0 +1,6 @@
+#include "sapceremoval.hpp"
+
+char * yartsev::spaceremoval(char * str) const 
+{
+  
+}
---
+++ yartsev.viktor/P3/spaceremoval.hpp
@@ -0,0 +1,8 @@
+#ifndef SPACEREMOVAL_HPP
+#define SPACEREMOVAL_HPP
+
+namespace yartsev {
+  char * spaceremoval(char * str) const;
+}
+
+#endif
