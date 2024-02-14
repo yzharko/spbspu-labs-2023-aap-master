0bb73f78d50947c967dc4f5f6e1d91602669bafd
Harunerd <harun.toksen45@gmail.com>
2024-02-14 21:14:07 +0300

    fixed main.cpp

--- tokshen.harun/P3/main.cpp
+++ tokshen.harun/P3/main.cpp
@@ -3,31 +3,58 @@
 #include "lat_two.hpp"
 #include <cstddef>
 #include <cstring>
+#include <stdexcept>
 
 int main() {
-
   size_t length = 2000;
   char* str1 = new char[length + 1];
   char* str2 = new char[length + 1];
+  char* temp = nullptr;
+
   std::cout << "Enter 1st string: " << "\n";
-  std::cin.getline(str1, length + 1);
+  try {
+    std::cin.getline(str1, length + 1);
 
-  size_t inputLength = strlen(str1);
+    if (std::cin.fail()) {
+      throw std::runtime_error("Input overflow or other error while reading first string.");
+    }
 
-  char* temp = new char[inputLength + 1];
+    size_t inputLength = strlen(str1);
 
-  toksen::convertToLower(str1, temp, inputLength);
+    if (inputLength > length) {
+      throw std::runtime_error("Input length error");
+    }
 
-  std::cout << temp << std::endl;
+    temp = new char[inputLength + 1];
 
-  std::cout << "Enter second string : " << "\n";
-  std::cin.getline(str2, length + 1);
-  char* mergeString = toksen::mergeLatinLetters(temp, str2);
-  std::cout << mergeString;
+    toksen::convertToLower(str1, temp, inputLength);
 
+    std::cout << temp << std::endl;
+
+    std::cout << "Enter second string : " << "\n";
+    std::cin.getline(str2, length + 1);
+
+    if (std::cin.fail()) {
+      throw std::runtime_error("Input overflow or other error while reading first string.");
+    }
+
+    char* mergeString = toksen::mergeLatinLetters(temp, str2);
+    std::cout << mergeString;
+    delete[] mergeString;
+  }
+  
+  catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    delete[] temp;
+    delete[] str1;
+    delete[] str2;
+    
+    return 1;
+    }
+
+  delete[] temp;
   delete[] str1;
   delete[] str2;
-  delete[] temp;
 
   return 0;
 }
