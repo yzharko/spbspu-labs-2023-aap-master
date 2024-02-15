ef56e49a02978814441948c01253caa195cc8c3d
yva <yartsev-va@yandex.ru>
2024-02-15 16:37:23 +0300

    Now processes cases in which nullptr is returned

--- yartsev.viktor/P3/main.cpp
+++ yartsev.viktor/P3/main.cpp
@@ -16,6 +16,12 @@ int main()
   char * ans_spc_rmv = yartsev::removeSpaces(str);
   const char * uni_str = "abacaba";
   char * ans_uni_two = yartsev::uniteStrings(str, uni_str);
+  if (ans_spc_rmv == nullptr) {
+    std::cerr << "Not enougth memory to store string for [SPC-RMV]\n";
+  }
+  if (ans_uni_two == nullptr) {
+    std::cerr << "Not enougth memory to store string for [UNI-TWO]\n";
+  }
   if (ans_spc_rmv != nullptr && ans_uni_two != nullptr) {
     std::cout << ans_spc_rmv << "\n";
     std::cout << ans_uni_two << "\n";
