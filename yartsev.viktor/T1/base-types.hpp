c7e7416d5522a9df02160df09bbbe9086353bdb1
yva <yartsev-va@yandex.ru>
2024-02-15 21:24:11 +0300

    Implemented base types

---
+++ yartsev.viktor/T1/base-types.hpp
@@ -0,0 +1,16 @@
+#ifndef BASE_TYPES_HPP
+#define BASE_TYPES_HPP
+
+namespace yartsev {
+  struct point_t {
+    double x_;
+    double y_;
+  };
+  struct rectangle_t {
+    double width_;
+    double height_;
+    double pos_;
+  };
+}
+
+#endif
