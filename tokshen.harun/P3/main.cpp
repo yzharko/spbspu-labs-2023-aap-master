157f4f48e924c6d365ab034f2be8c45a7794b0a6
Harunerd <harun.toksen45@gmail.com>
2024-02-20 22:16:01 +0300

    Fix CG's

--- tokshen.harun/P3/main.cpp
+++ tokshen.harun/P3/main.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 #include "upplow.hpp"
 #include "lat_two.hpp"
-#include <cstddef>
 #include <cstring>
 #include <stdexcept>
 
-int main() {
-
+int main() 
+{
   size_t length = 2000;
   char* str1 = new char[length + 1];
   char* str2 = new char[length + 1];
   char* temp = nullptr;
 
   std::cout << "Enter 1st string: " << "\n";
-  try {
+  try 
+  {
     std::cin.getline(str1, length + 1);
 
-    if (std::cin.fail()) {
+    if (std::cin.fail()) 
+    {
       throw std::runtime_error("Input overflow or other error while reading first string.");
     }
 
     size_t inputLength = strlen(str1);
 
-    if (inputLength > length) {
+    if (inputLength > length) 
+    {
       throw std::runtime_error("Input length error");
     }
 
@@ -32,21 +34,23 @@ int main() {
 
     std::cout << temp << std::endl;
 
-    std::cout << "Enter second string : " << "\n";
+    std::cout << "Enter second string : " << std::endl;
     std::cin.getline(str2, length + 1);
 
-    if (std::cin.fail()) {
-
+    if (std::cin.fail()) 
+    {
       std::cout << temp;
     }
-    else {
+    else 
+    {
       char* mergeString = toksen::mergeLatinLetters(temp, str2);
       std::cout << mergeString;
       delete[] mergeString;
     }
   }
 
-  catch (const std::exception& e) {
+  catch (const std::exception& e) 
+  {
     std::cerr << e.what() << std::endl;
     delete[] temp;
     delete[] str1;
--- tokshen.harun/P3/upplow.cpp
+++ tokshen.harun/P3/upplow.cpp
@@ -4,11 +4,12 @@
 
 namespace toksen
 {
-  void convertToLower(const char* source, char* destination, size_t size) {
-
-    for (size_t i = 0; i < size; i++) {
+  void convertToLower(const char* source, char* destination, size_t size) 
+  {
+    for (size_t i = 0; i < size; i++) 
+    {
       destination[i] = tolower(source[i]);
     }
       destination[size] = '\0';
-    }
+  }
 }
