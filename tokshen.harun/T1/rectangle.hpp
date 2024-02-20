90925e540bf8b7af1d32e87618f46bc19b76164b
Harunerd <harun.toksen45@gmail.com>
2024-02-20 23:30:49 +0300

    Fix 1.12

--- tokshen.harun/T1/rectangle.hpp
+++ tokshen.harun/T1/rectangle.hpp
@@ -8,20 +8,20 @@ namespace toksen
 {
   class Rectangle : public Shape
   {
-  public:
-    Rectangle(Point_t lowerLeftPoint, Point_t upperRightPoint);
-    ~Rectangle() override {};
-	double getArea() const override;
-	Rectangle_t getFrameRect() const override;
-	void move(const Point_t& offset) override;
-	void move(double offsetX, double offsetY) override;
-    void scale(double multiplier) override;
-  private:
-	Point_t lowerLeftPoint_;
-	Point_t upperRightPoint_;
-	Point_t getPos() const;
-	double getWidth() const;
-	double getHeight() const;
+    public:
+      Rectangle(Point_t lowerLeftPoint, Point_t upperRightPoint);
+      ~Rectangle() override {};
+	  double getArea() const override;
+	  Rectangle_t getFrameRect() const override;
+	  void move(const Point_t& offset) override;
+	  void move(double offsetX, double offsetY) override;
+      void scale(double multiplier) override;
+    private:
+	  Point_t lowerLeftPoint_;
+	  Point_t upperRightPoint_;
+	  Point_t getPos() const;
+	  double getWidth() const;
+	  double getHeight() const;
   };
 }
 
