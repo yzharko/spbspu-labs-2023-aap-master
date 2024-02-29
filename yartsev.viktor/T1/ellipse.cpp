e15973680226898d8ce44faa3ebe72365e57e01d
yva <yartsev-va@yandex.ru>
2024-02-29 15:29:45 +0300

    defined getArea()

--- yartsev.viktor/T1/ellipse.cpp
+++ yartsev.viktor/T1/ellipse.cpp
@@ -11,3 +11,9 @@ yartsev::Ellipse::Ellipse(point_t & center, double radiusX, double radiusY) :
   radiusX_(radiusX),
   radiusY_(radiusY)
 {}
+
+double yartsev::Ellipse::getArea() const
+{
+  double PI = 3.1415926535;
+  return PI * radiusX_ * radiusY_;
+}
