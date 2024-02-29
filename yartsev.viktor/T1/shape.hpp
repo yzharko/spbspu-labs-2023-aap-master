4f11181a3599e1420edc64ec9f70179f77d332cd
yva <yartsev-va@yandex.ru>
2024-02-29 15:34:40 +0300

    Same change

--- yartsev.viktor/T1/shape.hpp
+++ yartsev.viktor/T1/shape.hpp
@@ -6,7 +6,7 @@ namespace yartsev {
   class Shape {
     public:
       virtual double getArea() const = 0;
-      virtual yartsev::rectangle_t getFrameRect() = 0;
+      virtual rectangle_t getFrameRect() = 0;
       virtual void move(const point_t & to) = 0;
       virtual void move(const double & dx, const double & dy) = 0;
       virtual void scale(const double & scaling) = 0;
