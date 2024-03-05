c0e2eb5def48da34f156bfb39f8e08474e1fe704
Kepkkk21 <arseniy7405@gmail.com>
2024-03-05 05:46:25 +0300

    1

--- shapar.arseniy/P3/main.cpp
+++ shapar.arseniy/P3/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
-int main()
-{
-  const char * str = "asdfg";
+int main() {
 
+  char chr = 0;
+  size_t read = 0;
+  char * input = new char[10];
+  std::cin >> std::noskipws;
+  while ((std::cin >> chr) && (chr != '\n'))
+  {
+    input[read++] = chr;
+  }
+  std::cin >> std::skipws;
+  std::cout << input << "\n";
+  delete [] input;
+  return 0;
 }
